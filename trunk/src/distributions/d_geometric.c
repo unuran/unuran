3cbf6225427887dd272846d031056077f17a89c9
jleydold <josef.leydold@wu.ac.at>
2000-07-25 16:19:55 +0000

    added special generator for Zipf distribution
    svn path=/trunk/; revision=188

---
+++ trunk/src/distributions/d_geometric.c
@@ -0,0 +1,144 @@
+/*****************************************************************************
+ *                                                                           *
+ *          UNURAN -- Universal Non-Uniform Random number generator          *
+ *                                                                           *
+ *****************************************************************************
+ *                                                                           *
+ *   FILE:      d_geometric.c                                                *
+ *                                                                           *
+ *   REFERENCES:                                                             *
+ *                                                                           *
+ *   [1] N.L. Johnson, S. Kotz and A.W. Kemp                                 *
+ *       Univariate Discrete Distributions,                                  *
+ *       2nd edition                                                         *
+ *       John Wiley & Sons, Inc., New York, 1992                             *
+ *                                                                           *
+ *****************************************************************************
+ *****************************************************************************
+ *                                                                           *
+ *  Geometric distribution  [1; ch.5.2, p.201]                               *
+ *                                                                           *
+ *  pmf:       p(k) = p * (1-p)^k                                            *
+ *  domain:    0 <= k < infinity                                             *
+ *  constant:  1                                                             *
+ *                                                                           *
+ *  parameters:                                                              *
+ *     0:  0 < p < 1 ... shape                                               *
+ *                                                                           *
+ *****************************************************************************
+     $Id$
+ *****************************************************************************
+ *                                                                           *
+ *   Copyright (c) 2000 Wolfgang Hoermann and Josef Leydold                  *
+ *   Dept. for Statistics, University of Economics, Vienna, Austria          *
+ *                                                                           *
+ *   This program is free software; you can redistribute it and/or modify    *
+ *   it under the terms of the GNU General Public License as published by    *
+ *   the Free Software Foundation; either version 2 of the License, or       *
+ *   (at your option) any later version.                                     *
+ *                                                                           *
+ *   This program is distributed in the hope that it will be useful,         *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ *   GNU General Public License for more details.                            *
+ *                                                                           *
+ *   You should have received a copy of the GNU General Public License       *
+ *   along with this program; if not, write to the                           *
+ *   Free Software Foundation, Inc.,                                         *
+ *   59 Temple Place, Suite 330, Boston, MA 02111-1307, USA                  *
+ *                                                                           *
+ *****************************************************************************/
+
+/*---------------------------------------------------------------------------*/
+
+#include <source_unuran.h>
+#include <source_distributions.h>
+
+/*---------------------------------------------------------------------------*/
+
+static const char distr_name[] = "geometric";
+
+/* parameters */
+#define p  params[0]
+
+/* function prototypes                                                       */
+static double _unur_pmf_geometric(int k, double *params, int n_params);
+/*  static double _unur_cdf_geometric(int k, double *params, int n_params); */
+
+/*---------------------------------------------------------------------------*/
+
+double
+_unur_pmf_geometric(int k, double *params, int n_params)
+{ 
+  return ((k<0) ? 0. : p * pow( 1.-p, k );
+} /* end of _unur_pmf_geometric() */
+
+/*---------------------------------------------------------------------------*/
+
+struct unur_distr *
+unur_distr_geometric( double *params, int n_params )
+{
+#define DISTR distr->data.discr
+  register struct unur_distr *distr;
+
+  /* check new parameter for generator */
+  if (n_params < 1) {
+    _unur_error(distr_name,UNUR_ERR_DISTR_NPARAMS,"too few"); return NULL; }
+  if (n_params > 1)
+    _unur_warning(distr_name,UNUR_ERR_DISTR_NPARAMS,"too many");
+  CHECK_NULL(params,NULL);
+
+  /* get new (empty) distribution object */
+  distr = unur_distr_discr_new();
+
+  /* set distribution id */
+  distr->id = UNUR_DISTR_GEOMETRIC;
+
+  /* name of distribution */
+  distr->name = distr_name;
+             
+  /* how to get special generators */
+  DISTR.init = _unur_stdgen_geometric_init;
+   
+  /* functions */
+  DISTR.pmf  = _unur_pmf_geometric;   /* pointer to p.m.f.            */
+  /* DISTR.cdf  = _unur_cdf_geometric;   pointer to c.d.f.            */
+
+  /* copy parameters */
+  DISTR.p = p;
+
+  /* check parameters */
+  if (DISTR.p <= 0. || DISTR.p >= 1.) {
+    _unur_error(distr_name,UNUR_ERR_DISTR_DOMAIN,"p <= 0 || p >= 1");
+    free( distr ); return NULL;
+  }
+
+  /* number of arguments */
+  DISTR.n_params = n_params;
+
+  /* log of normalization constant */
+
+  /* mode and area below p.d.f. */
+  /*    DISTR.mode = 0.; */
+  DISTR.area = 1.;
+
+  /* domain */
+  DISTR.domain[0] = 0.;          /* left boundary  */
+  DISTR.domain[1] = INFINITY;    /* right boundary */
+
+  /* indicate which parameters are set */
+  distr->set = ( UNUR_DISTR_SET_PARAMS | 
+		 UNUR_DISTR_SET_DOMAIN |
+		 UNUR_DISTR_SET_STDDOMAIN |
+                 /* UNUR_DISTR_SET_MODE   |  */
+		 UNUR_DISTR_SET_PDFAREA );
+                
+  /* return pointer to object */
+  return distr;
+
+#undef DISTR
+} /* end of unur_distr_geometric() */
+
+/*---------------------------------------------------------------------------*/
+#undef nu
+/*---------------------------------------------------------------------------*/
---
+++ trunk/src/distributions/d_geometric_gen.c
@@ -0,0 +1,157 @@
+/*****************************************************************************
+ *                                                                           *
+ *          UNURAN -- Universal Non-Uniform Random number generator          *
+ *                                                                           *
+ *****************************************************************************
+ *                                                                           *
+ *   FILE:      d_geometric_gen.c                                            *
+ *                                                                           *
+ *   Special generators for Geometric distribution                           *
+ *                                                                           *
+ *****************************************************************************
+     $Id$
+ *****************************************************************************
+ *                                                                           *
+ *   Copyright (c) 2000 Wolfgang Hoermann and Josef Leydold                  *
+ *   Dept. for Statistics, University of Economics, Vienna, Austria          *
+ *                                                                           *
+ *   This program is free software; you can redistribute it and/or modify    *
+ *   it under the terms of the GNU General Public License as published by    *
+ *   the Free Software Foundation; either version 2 of the License, or       *
+ *   (at your option) any later version.                                     *
+ *                                                                           *
+ *   This program is distributed in the hope that it will be useful,         *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ *   GNU General Public License for more details.                            *
+ *                                                                           *
+ *   You should have received a copy of the GNU General Public License       *
+ *   along with this program; if not, write to the                           *
+ *   Free Software Foundation, Inc.,                                         *
+ *   59 Temple Place, Suite 330, Boston, MA 02111-1307, USA                  *
+ *                                                                           *
+ *****************************************************************************/
+
+/*---------------------------------------------------------------------------*/
+
+#include <source_unuran.h>
+#include <source_distributions.h>
+
+/*---------------------------------------------------------------------------*/
+/* init routines for special generators                                      */
+
+/*---------------------------------------------------------------------------*/
+/* abbreviations */
+
+#define PAR       par->data.dstd        /* data for parameter object         */
+#define GEN       gen->data.dstd        /* data for generator object         */
+#define DISTR     gen->distr.data.discr /* data for distribution in generator object */
+
+#define uniform()  _unur_call_urng(gen) /* call for uniform prng             */
+
+/* parameters */
+#define p  (DISTR.params[0])    /* shape */
+
+/*---------------------------------------------------------------------------*/
+
+/*****************************************************************************/
+/**                                                                         **/
+/**  Inititialize                                                           **/
+/**                                                                         **/
+/*****************************************************************************/
+
+/*---------------------------------------------------------------------------*/
+
+int 
+_unur_stdgen_geometric_init( struct unur_par *par, struct unur_gen *gen )
+     /*----------------------------------------------------------------------*/
+     /* initialize special generator for Geometric distribution              */
+     /* if gen == NULL then only check existance of variant.                 */
+     /*                                                                      */
+     /* parameters:                                                          */
+     /*   par ... pointer to parameter for building generator object         */
+     /*   gen ... pointer to generator object                                */
+     /*                                                                      */
+     /* return:                                                              */
+     /*   1 ... on success                                                   */
+     /*   0 ... on error                                                     */
+     /*----------------------------------------------------------------------*/
+{
+  /* check arguments */
+  CHECK_NULL(par,0.);
+  COOKIE_CHECK(par,CK_DSTD_PAR,0.);
+
+  switch (par->variant) {
+
+  case 0:  /* DEFAULT */
+  case UNUR_STDGEN_INVERSION:   /* inversion method */
+    _unur_dstd_set_sampling_routine( par,gen,unur_stdgen_sample_geometric_inv );
+    PAR.is_inversion = TRUE;
+    return 1;
+
+  default: /* no such generator */
+    if (gen) _unur_warning(par->genid,UNUR_ERR_SHOULD_NOT_HAPPEN,"");
+    return 0;
+  }
+  
+} /* end of _unur_stdgen_geometric_init() */
+
+/*---------------------------------------------------------------------------*/
+
+/*****************************************************************************/
+/**                                                                         **/
+/**  Special generators                                                     **/
+/**                                                                         **/
+/*****************************************************************************/
+
+/*---------------------------------------------------------------------------*/
+
+/*****************************************************************************
+ *                                                                           *
+ * Geometric Distribution: Inversion                                         *
+ *                                                                           *
+ *****************************************************************************
+ *                                                                           *
+ * FUNCTION:   - samples a random number from the Geometric distribution     *
+ *               with parameter 0 < p < 1.                                   *
+ *                                                                           *
+ * Implemented by R.Kremer 1990, revised by P.Busswald, July 1992            *
+ *****************************************************************************
+ *                                                                           *
+ *  On generating random numbers of a discrete distribution by Inversion     *
+ *  normally sequential search is necessary, but in the case of the          *
+ *  Geometric distribution a direct transformation is possible because of    *
+ *  the special parallel to the continuous Exponential distribution Exp(t):  *
+ *    X - Exp(t): G(x)=1-exp(-tx)                                            *
+ *        Geo(p): pk=G(k+1)-G(k)=exp(-tk)*(1-exp(-t))                        *
+ *                p=1-exp(-t)                                                *
+ *  A random number of the Geometric distribution Geo(p) is obtained by      *
+ *  k = (int)x, where x is from Exp(t) with parameter t = -log(1-p).         *
+ *                                                                           *
+ *****************************************************************************
+ *    WinRand (c) 1995 Ernst Stadlober, Institut fuer Statistitk, TU Graz    *
+ *****************************************************************************/
+
+/*---------------------------------------------------------------------------*/
+
+int
+unur_stdgen_sample_geometric_inv( struct unur_gen *gen )
+{
+  /* -X- generator code -X- */
+  double U;
+  int K;
+
+  /* sample from uniform random number generator */
+/*    while ((U = GEN.umin + uniform() * (GEN.umax-GEN.umin)) == 0.); */
+  while ((U = uniform()) == 0.);
+
+  /* transform to random variate */
+  K = (int) (log(U) / log(1.-p));
+
+  /* -X- end of generator code -X- */
+
+  return K;
+  
+} /* end of unur_stdgen_sample_geometric_inv() */
+
+/*---------------------------------------------------------------------------*/

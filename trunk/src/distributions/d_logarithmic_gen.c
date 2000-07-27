b746ce9a7560e7b022689076b82397139e34c72b
jleydold <josef.leydold@wu.ac.at>
2000-07-27 10:17:35 +0000

    fixed typo
    svn path=/trunk/; revision=209

--- trunk/src/distributions/d_logarithmic_gen.c
+++ trunk/src/distributions/d_logarithmic_gen.c
@@ -182,12 +182,8 @@ unur_stdgen_sample_logarithmic_lsk( struct unur_gen *gen )
   double U, V, p, q;
   int K;
 
-
-
   /* check arguments */
   CHECK_NULL(gen,0.); COOKIE_CHECK(gen,CK_DSTD_GEN,0.);
-
-
 
   U = uniform();
 

858e40d9abd8fbd0e04a93f675c3017e307e8af4
jleydold <josef.leydold@wu.ac.at>
2000-07-31 14:04:52 +0000

    bug fix
    svn path=/trunk/; revision=258

--- trunk/src/methods/dau.c
+++ trunk/src/methods/dau.c
@@ -571,7 +571,7 @@ _unur_dau_debug_init( struct unur_par *par, struct unur_gen *gen )
 
   /* check arguments */
   CHECK_NULL(par,/*void*/);  COOKIE_CHECK(par,CK_DAU_PAR,/*void*/);
-  CHECK_NULL(gen,/*void*/);  COOKIE_CHECK(par,CK_DAU_GEN,/*void*/);
+  CHECK_NULL(gen,/*void*/);  COOKIE_CHECK(gen,CK_DAU_GEN,/*void*/);
 
   log = unur_get_stream();
 

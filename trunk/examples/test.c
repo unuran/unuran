4c8789ea323b1c5252ebc2d7c4dc351a4d49cf98
janka <janka@localhost>
2000-09-28 07:54:10 +0000

    *** empty log message ***
    svn path=/trunk/; revision=380

--- trunk/examples/test.c
+++ trunk/examples/test.c
@@ -1,8 +1,9 @@
-/*  my second UNURAN program test2.c */ 
+/*   my second UNURAN program test2.c */ 
 
 #include <unuran.h>
 #include <unuran_distributions.h>
 #include <unuran_tests.h>
+
 
 double trig (double x)
 {  return ( (x<0.) ? 0. : (x>1.) ? 1. : x ); }  
@@ -14,47 +15,54 @@ double dpdftrig (double x)
 
 int main()
 {
-  double x;
+  double x, x1;
   int    i;
+  double q0, q1, q2, q3, q4;
 
-  struct unur_distr *distr,*distr1;  /* distribution */ 
-  struct unur_par *par,*par1;      /* parameter */
-  struct unur_gen *gen,*gen1;      /* generator */
+  UNUR_DISTR  *distr,*distr1;    /* distribution */ 
+  UNUR_PAR    *par,  *par1;      /* parameter */
+  UNUR_GEN    *gen,  *gen1;      /* generator */
 /* mache neues Verteilungsobjekt unur_distr_cont_new (methods.distr.c)   */
 /*     setze pointer auf pdf und cdf  */
  
   /* make distr. object: truncated Gaussian */
+    distr  = unur_distr_normal(NULL,0);
     distr1 = unur_distr_normal(NULL,0);
-  //distr1 = unur_distr_cont_new();
-  //unur_distr_cont_set_cdf(distr1, trig );
-  //unur_distr_cont_set_pdf(distr1, pdftrig );  
-  //unur_distr_cont_set_dpdf(distr1,dpdftrig );  
-  // unur_distr_cont_set_mode(distr1,0.5 );
-
-  // unur_distr_cont_set_domain(distr, -2.,2.);
-
+ //distr1 = unur_distr_cont_new();
+ 
   /* choose method and set parameters */
-  //  par = unur_arou_new(distr);
-   par1 = unur_ninv_new(distr1);
-   unur_ninv_use_regula(par1);
-   unur_ninv_use_table(par1);
-  // unur_arou_set_max_sqhratio(par,0.99);
+    par  = unur_ninv_new(distr);
+    par1 = unur_ninv_new(distr1);
+    unur_set_debug(par,  0);
+    unur_set_debug(par1, 0);
+
 
   /* make generator object */
-  //  gen = unur_init(par);
-  gen1 = unur_init(par1);
-  if (gen1 ==NULL){
-    printf("NULL Pointer bei Generatorobjekt\n");
-    exit(1);
-  }
+    gen  = unur_init(par);
+    gen1 = unur_init(par1);
+    if (gen1 == NULL){
+      printf("NULL Pointer bei Generatorobjekt\n");
+      exit(1);
+    }
 
   /* sample */
+ 
   for (i=0;i<100;i++) {
-  x = unur_sample_cont(gen1);
-  //printf("%f\n",x);
+  x  = unur_sample_cont(gen);
+  x1 = unur_sample_cont(gen1);
+  printf("%f %f \n",x, x1);
   }
  
 
+  /* test of PP-algorithm in quantile.c */  
+  q0 = 0.;
+  q1 = 0.;
+  q2 = 0.;
+  q3 = 0.;
+  q4 = 0.; 
+  unur_test_quartiles( gen1, &q0 ,&q1, &q2, &q3, &q4, 1000 );
+
+  unur_test_correlation(gen, gen1, 10000);
 
   /* destroy generator object */
   // unur_free(gen);
@@ -62,6 +70,10 @@ int main()
 
   exit (0);
 }
+
+
+
+
 
 
 

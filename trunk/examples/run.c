9766ab2e314709a9f71da935eed4d424eece9e15
jleydold <josef.leydold@wu.ac.at>
2000-11-21 10:14:51 +0000

    run.c
    svn path=/trunk/; revision=559

--- trunk/examples/run.c
+++ trunk/examples/run.c
@@ -23,25 +23,52 @@ int main()
 
   UNUR_DISTR *distr;    /* distribution */
   UNUR_PAR   *par;      /* parameter */
-  UNUR_GEN   *gen;      /* generator */
+  //  UNUR_GEN   *gen;      /* generator */
   UNUR_URNG *urng;      
-
-  double data[] = {2.,6.,1.,9.,15.,14.,4.,3.,8.,11.,5.,0.,7.};
 
   urng = prng_new("mt19937(2345)");
   if (!urng) exit(-1);
 
+  unur_set_default_debug(0);
   unur_set_default_urng(urng);
   
-  distr = unur_distr_new(UNUR_DISTR_CEMP);
-  unur_distr_cemp_set_data( distr, data, 13 );
+  distr = unur_distr_normal(NULL,0);
 
-  par = unur_empk_new( distr );
-  unur_empk_set_kernel(par, UNUR_DISTR_GAUSSIAN);
-
-  gen = unur_init( par );
-
-  unur_test_printsample(gen,10,8);
+  par = unur_tdr_new(distr);
+  unur_tdr_set_c(par,0.);
+  unur_tdr_set_cpoints(par,40,NULL);
+  unur_tdr_set_variant_gw(par);
+  unur_run_tests(par,~0u);
+  
+  par = unur_tdr_new(distr);
+  unur_tdr_set_c(par,0.);
+  unur_tdr_set_cpoints(par,40,NULL);
+  unur_tdr_set_variant_ps(par);
+  unur_run_tests(par,~0u);
+  
+  par = unur_tdr_new(distr);
+  unur_tdr_set_c(par,0.);
+  unur_tdr_set_cpoints(par,40,NULL);
+  unur_tdr_set_variant_ia(par);
+  unur_run_tests(par,~0u);
+  
+  par = unur_tdr_new(distr);
+  unur_tdr_set_c(par,-0.5);
+  unur_tdr_set_cpoints(par,40,NULL);
+  unur_tdr_set_variant_gw(par);
+  unur_run_tests(par,~0u);
+  
+  par = unur_tdr_new(distr);
+  unur_tdr_set_c(par,-0.5);
+  unur_tdr_set_cpoints(par,40,NULL);
+  unur_tdr_set_variant_ps(par);
+  unur_run_tests(par,~0u);
+  
+  par = unur_tdr_new(distr);
+  unur_tdr_set_c(par,-0.5);
+  unur_tdr_set_cpoints(par,40,NULL);
+  unur_tdr_set_variant_ia(par);
+  unur_run_tests(par,~0u);
   
 
   exit (0);

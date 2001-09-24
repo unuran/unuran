0cf133fe1ecef8eb506224dfa55a2d7f6dbbdd61
janka <janka@localhost>
2001-09-24 11:57:41 +0000

    names adapted for naming scheme
    svn path=/trunk/; revision=1100

--- trunk/run/parsetest.c
+++ trunk/run/parsetest.c
@@ -8,9 +8,9 @@
 extern void         subst_whitespace(char *, char);
 extern char       * elim_whitespace(char *);
 extern int          parselist(char *, double *);
-extern UNUR_DISTR * make_distr_obj(char *);
-extern UNUR_PAR   * make_par_obj(UNUR_DISTR *, char *);
-extern UNUR_GEN   * make_gen_obj (char *);
+extern UNUR_DISTR * _unur_str2distr(char *);
+extern UNUR_PAR   * _unur_str2par(UNUR_DISTR *, char *);
+extern UNUR_GEN   * unur_str2gen(char *);
 
 /* main -- test program */
 int main(){
@@ -23,28 +23,28 @@ int main(){
   /* test1 */
   strcpy(str, "distr=normal(0, 1):prng = MT19937(123): method=tdr; c=-0.5; variant_ia");
   printf("%s\n", str);
-  gen = make_gen_obj(str);
+  gen = unur_str2gen(str);
   for ( i=0; i<5; i++){
     printf("rand num: %f\n", unur_sample_cont(gen));
   }
   /* test2 */
   strcpy(str, "distr=normal(0, 1): method=ninv;");
   printf("%s\n", str);
-  gen = make_gen_obj(str);
+  gen = unur_str2gen(str);
   for ( i=0; i<5; i++){
     printf("rand num: %f\n", unur_sample_cont(gen));
   }
   /* test3 */
   strcpy(str, "distr=normal(0, 1):prng = MT19937(6)");
   printf("%s\n", str);
-  gen = make_gen_obj(str);
+  gen = unur_str2gen(str);
   for ( i=0; i<5; i++){
     printf("rand num: %f\n", unur_sample_cont(gen));
   }
   /* test4 */
-  strcpy(str, "distr=beta(1, 1): method=dau; c=-0.5; variant_ia:prng=MT19937(454)");
+  strcpy(str, "distr=beta(1, 1): method=ninv; c=-0.5; variant_ia:prng=MT19937(454)");
   printf("%s\n", str);
-  gen = make_gen_obj(str);
+  gen = unur_str2gen(str);
   for ( i=0; i<5; i++){
     printf("rand num: %f\n", unur_sample_cont(gen));
   }

60ea6307b16981c34ab8f462874391908f5da78f
jleydold <josef.leydold@wu.ac.at>
2001-10-17 11:08:28 +0000

    minor changes
    svn path=/trunk/; revision=1145

--- trunk/run/parsetest.c
+++ trunk/run/parsetest.c
@@ -23,7 +23,7 @@ int main(){
     printf("rand num: %f\n", unur_sample_cont(gen));
   }
   /* test2 */
-  strcpy(str, "distr=normal(0, 1);domain=(1,3): method=ninv;");
+  strcpy(str, "daksdjfkljistr=normal(0, 1);domain=(1,3): method=ninv;");
   printf("%s\n", str);
   gen = unur_str2gen(str);
   for ( i=0; i<5; i++){

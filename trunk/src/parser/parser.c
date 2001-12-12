d5ca7b12d1b8382401b0c553b2e4315adfd0083c
jleydold <josef.leydold@wu.ac.at>
2001-12-12 14:10:37 +0000

    added file file common parse routines
    svn path=/trunk/; revision=1213

---
+++ trunk/src/parser/parser.c
@@ -0,0 +1,90 @@
+/*****************************************************************************
+ *                                                                           *
+ *          UNURAN -- Universal Non-Uniform Random number generator          *
+ *                                                                           *
+ *****************************************************************************
+ *                                                                           *
+ *   FILE:      parser.c                                                     *
+ *                                                                           *
+ *   common routines for parsing strings.                                    *
+ *                                                                           *
+ *****************************************************************************
+ *                                                                           *
+ *   REMARK:                                                                 *
+ *   The parser always uses the default debugging flag.                      *
+ *                                                                           *
+ *****************************************************************************
+     $Id$
+ *****************************************************************************
+ *                                                                           *
+ *   Copyright (c) 2000 Wolfgang Hoermann and Josef Leydold                  *
+ *   Dept. for Statistics, University of Economics, Vienna, Austria          *
+ *                                                                           *
+ *   This program is free software; you can redistribute it and/or modify    *
+ *   it under the terms of the GNU General Public License as published by    *
+ *   the Free Software Foundation; either version 2 of the License, or       *
+ *   (at your option) any later version.                                     *
+ *                                                                           *
+ *   This program is distributed in the hope that it will be useful,         *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ *   GNU General Public License for more details.                            *
+ *                                                                           *
+ *   You should have received a copy of the GNU General Public License       *
+ *   along with this program; if not, write to the                           *
+ *   Free Software Foundation, Inc.,                                         *
+ *   59 Temple Place, Suite 330, Boston, MA 02111-1307, USA                  *
+ *                                                                           *
+ *****************************************************************************/
+
+/*---------------------------------------------------------------------------*/
+
+#include <ctype.h>
+#include <source_unuran.h>
+
+/*---------------------------------------------------------------------------*/
+
+char *
+_unur_parser_prepare_string( const char *str )
+     /*----------------------------------------------------------------------*/
+     /* Prepare string for processing:                                       */
+     /*   Make a working copy of the string.                                 */
+     /*   Remove all white spaces and convert to lower case letters.         */
+     /*                                                                      */
+     /* parameters:                                                          */
+     /*   str      ... pointer to string                                     */
+     /*                                                                      */
+     /* return:                                                              */
+     /*   pointer to working string.                                         */
+     /*                                                                      */
+     /* as a side effect, a new string is allocated.                         */
+     /*----------------------------------------------------------------------*/
+{
+  char *tmp, *ptr;
+  char *new;       /* pointer to working copy of string */
+  size_t len;      /* length of string */
+
+  /* length of string */
+  len = strlen(str)+1;
+  /* allocate memory for copy */
+  new = _unur_malloc( len * sizeof(char) );
+  /* copy memory */
+  ptr = memcpy(new,str,len);
+
+  /* copy characters but skip all white spaces */
+  for (tmp = ptr; *tmp != '\0'; tmp++)
+    if ( !isspace(*tmp) ) {
+      *ptr = tolower(*tmp);
+      ptr++;
+    }
+
+  /* terminate string */
+  *ptr = '\0';
+
+  /* return pointer to working copy */
+  return new;
+
+} /* end of _unur_parser_prepare_string() */
+
+/*---------------------------------------------------------------------------*/
+

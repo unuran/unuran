f0ed1a854d3b4250ea515be6d1768e1cfc846557
jleydold <josef.leydold@wu.ac.at>
2002-01-02 15:30:25 +0000

    added some error checking with function tree evaluation
    svn path=/trunk/; revision=1233

--- trunk/src/parser/functparser_eval.c
+++ trunk/src/parser/functparser_eval.c
@@ -63,40 +63,50 @@ _unur_fstr_eval_tree (const struct ftreenode *root, const double x)
 /*****************************************************************************/
 
 /*---------------------------------------------------------------------------*/
+#define CHECK_INF(x)    if(_unur_FP_is_infinity((x))) return INFINITY;
+#define CHECK_INFS(l,r) do { CHECK_INF((l)); CHECK_INF((r)); } while(0)
+/*---------------------------------------------------------------------------*/
 
 double v_dummy  (double l, double r) { return 0.; }
 double v_const  (double l, double r) { return 0.; }  /* nothing to do, value in node */
 
-double v_less   (double l, double r) { return (double)(l <  r); }
-double v_equal  (double l, double r) { return (double)(l == r); }
-double v_greater(double l, double r) { return (double)(l >  r); }
-double v_less_or(double l, double r) { return (double)(l <= r); }
-double v_unequal(double l, double r) { return (double)(l != r); }
-double v_grtr_or(double l, double r) { return (double)(l >= r); }
+double v_less   (double l, double r) { CHECK_INFS(l,r); return (double)(l <  r); }
+double v_equal  (double l, double r) { CHECK_INFS(l,r); return (double)(l == r); }
+double v_greater(double l, double r) { CHECK_INFS(l,r); return (double)(l >  r); }
+double v_less_or(double l, double r) { CHECK_INFS(l,r); return (double)(l <= r); }
+double v_unequal(double l, double r) { CHECK_INFS(l,r); return (double)(l != r); }
+double v_grtr_or(double l, double r) { CHECK_INFS(l,r); return (double)(l >= r); }
 
-double v_or     (double l, double r) { return (double)((int)l || (int)r);  }
+double v_or     (double l, double r) { CHECK_INFS(l,r); return (double)((int)l || (int)r);  }
 double v_xor    (double l, double r) { return (double)((l==0 && r!=0) || (l!=0 && r== 0)); }
-double v_and    (double l, double r) { return (double)((int)l && (int)r); }
-double v_not    (double l, double r) { return (double)(!((int)r))   ; }
+double v_and    (double l, double r) { CHECK_INFS(l,r); return (double)((int)l && (int)r); }
+double v_not    (double l, double r) { CHECK_INF(r);    return (double)(!((int)r)); }
 
-double v_plus   (double l, double r) { return (l + r); }
-double v_minus  (double l, double r) { return (l - r); }
-double v_mul    (double l, double r) { return (l * r); }
-double v_div    (double l, double r) { return (l / r); }
-double v_power  (double l, double r) { return pow(l,r); }
+double v_plus   (double l, double r) { CHECK_INFS(l,r); return (l + r); }
+double v_minus  (double l, double r) { CHECK_INFS(l,r); return (l - r); }
+double v_mul    (double l, double r) { CHECK_INFS(l,r); return (l * r); }
+double v_div    (double l, double r) { CHECK_INFS(l,r); if (r==0.) return INFINITY;
+                                                        return (l / r); }
+double v_power  (double l, double r) { CHECK_INFS(l,r); return pow(l,r); }
 
-double v_mod    (double l, double r) { return (double)((int)l % (int)r); }
-double v_exp    (double l, double r) { return exp(r); }
-double v_ln     (double l, double r) { return log(r); }
-double v_log    (double l, double r) { return log(r)/log(l); }
-double v_sin    (double l, double r) { return sin(r); }
-double v_cos    (double l, double r) { return cos(r); }
-double v_tan    (double l, double r) { return tan(r); }
-double v_sec    (double l, double r) { return 1./cos(r); }
-double v_sqrt   (double l, double r) { return sqrt(r); }
-double v_abs    (double l, double r) { return abs(r); }
-double v_sgn    (double l, double r) { return ((r<0.) ? -1. : ((r>0.) ? 1. : 0.)); }
+double v_mod    (double l, double r) { CHECK_INFS(l,r); return (double)((int)l % (int)r); }
+double v_exp    (double l, double r) { CHECK_INF(r);    return exp(r); }
+double v_ln     (double l, double r) { CHECK_INF(r);    if(r<=0.) return INFINITY; return log(r); }
+double v_log    (double l, double r) { CHECK_INFS(l,r); if(r<=0.||l<=0.||l==1.) return INFINITY;
+                                                        return log(r)/log(l); }
+double v_sin    (double l, double r) { CHECK_INF(r);    return sin(r); }
+double v_cos    (double l, double r) { CHECK_INF(r);    return cos(r); }
+double v_tan    (double l, double r) { CHECK_INF(r);    return tan(r); }
+double v_sec    (double l, double r) { double cosr; CHECK_INF(r); if((cosr=cos(r))==0.) return INFINITY;
+                                                        return 1./cosr; }
+double v_sqrt   (double l, double r) { CHECK_INF(r);    if(r<0.) return INFINITY;
+                                                        return sqrt(r); }
+double v_abs    (double l, double r) { CHECK_INF(r);    return fabs(r); }
+double v_sgn    (double l, double r) { CHECK_INF(r);    return ((r<0.) ? -1. : ((r>0.) ? 1. : 0.)); }
 
+/*---------------------------------------------------------------------------*/
+#undef CHECK_INF
+#undef CHECK_INFS
 /*---------------------------------------------------------------------------*/
 
 /*****************************************************************************/

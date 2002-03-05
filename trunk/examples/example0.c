05a57a570025fbf123a8f5b7c0a16d6714faa10d
jleydold <josef.leydold@wu.ac.at>
2002-03-05 16:16:34 +0000

    added example for method AUTO
    svn path=/trunk/; revision=1278

---
+++ trunk/examples/example0.c
@@ -0,0 +1,69 @@
+/* ------------------------------------------------------------- */
+/* File: example0.c                                              */
+/* ------------------------------------------------------------- */
+
+/* Include UNURAN header file.                                   */
+#include <unuran.h>
+
+/* ------------------------------------------------------------- */
+
+int main()
+{
+  int    i;     /* loop variable                                 */
+  double x;     /* will hold the random number                   */
+
+  /* Declare the three UNURAN objects.                           */
+  UNUR_DISTR *distr;    /* distribution object                   */
+  UNUR_PAR   *par;      /* parameter object                      */
+  UNUR_GEN   *gen;      /* generator object                      */
+
+  /* Use a predefined standard distribution:                     */
+  /*   Gaussian with mean zero and standard deviation 1.         */
+  /*   Since this is the standard form of the distribution,      */
+  /*   we need not give these parameters.                        */
+  distr = unur_distr_normal(NULL, 0);
+
+  /* Use method AUTO:                                            */
+  /*   Let UNURAN select a suitable method for you.              */
+  par = unur_auto_new(distr);
+
+  /* Now you can change some of the default settings for the     */
+  /* parameters of the chosen method. We don't do it here.       */
+
+  /* Create the generator object.                                */
+  gen = unur_init(par);
+
+  /* Notice that this call has also destroyed the parameter      */
+  /* object `par' as a side effect.                              */
+
+  /* It is important to check if the creation of the generator   */
+  /* object was successful. Otherwise `gen' is the NULL pointer  */ 
+  /* and would cause a segmentation fault if used for sampling.  */
+  if (gen == NULL) {
+     fprintf(stderr, "ERROR: cannot create generator object\n");
+     exit (EXIT_FAILURE);
+  }
+
+  /* It is possible to reuse the distribution object to create   */
+  /* another generator object. If you do not need it any more,   */
+  /* it should be destroyed to free memory.                      */
+  unur_distr_free(distr);
+
+  /* Now you can use the generator object `gen' to sample from   */
+  /* the standard Gaussian distribution.                         */
+  /* Eg.:                                                        */
+  for (i=0; i<10; i++) {
+    x = unur_sample_cont(gen);
+    printf("%f\n",x);
+  }
+
+  /* When you do not need the generator object any more, you     */
+  /* can destroy it.                                             */
+  unur_free(gen);
+
+  exit (EXIT_SUCCESS);
+
+} /* end of main() */
+
+/* ------------------------------------------------------------- */
+

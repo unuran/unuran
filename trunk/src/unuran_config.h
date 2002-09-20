7f99b8eec209411fc6d9601329c7e696ba712c5f
jleydold <josef.leydold@wu.ac.at>
2002-09-20 09:21:20 +0000

    file reorganized, docu for URNG interfaces added
    svn path=/trunk/; revision=1360

--- trunk/src/unuran_config.h
+++ trunk/src/unuran_config.h
@@ -37,6 +37,120 @@
 #define __UNURAN_CONFIG_H_SEEN
 /*---------------------------------------------------------------------------*/
 
+/*****************************************************************************
+ *  Logging and debugging.                                                   *
+ *****************************************************************************/
+
+/*---------------------------------------------------------------------------*/
+/* Default name of log file.                                                 */
+/*                                                                           */
+/* Use "stdout" to write all infos to stdout.                                */
+/* If no log file should be used, #undef the macro UNUR_ENABLE_LOGGING below.*/
+
+#define UNUR_LOG_FILE "unuran.log"
+/*  #define UNUR_LOG_FILE "stdout" */
+
+/*---------------------------------------------------------------------------*/
+/* Define this macro to switch on writing information about the              */
+/* generator into log file.                                                  */
+/*                                                                           */
+/* If no log file should be used at all, #undef this macro.                  */
+
+#define UNUR_ENABLE_LOGGING 1
+
+/*---------------------------------------------------------------------------*/
+/* Every message about a generator that is written into the log file or      */
+/* or to the stderr starts with an identifier. Every generator has its own   */
+/* unique identifier (well if there are not more than 999 generators).       */
+/* It is composed by the generator type, followed by a dot and three digits. */
+/* Building such a generator id can be disabled by undefining the following  */
+/* macro. If it is disabled the generator type (a constant string) is used   */
+/* for the identifier (which is not unique any more if more than one         */
+/* generator of the same type is created).                                   */
+
+#define UNUR_ENABLE_GENID  1
+
+/*---------------------------------------------------------------------------*/
+/* Set default flag for debugging of generators:                             */
+/*                                                                           */
+/*   UNUR_DEBUG_OFF    ... switch off debugging information                  */
+/*   UNUR_DEBUG_INIT   ... pameters and structure of generator only          */
+/*   UNUR_DEBUG_SETUP  ... information for setup step                        */
+/*   UNUR_DEBUG_ADAPT  ... trace adaptive steps                              */ 
+/*   UNUR_DEBUG_SAMPLE ... trace sampling                                    */
+/*   UNUR_DEBUG_ALL    ... write all available debugging information         */
+/*                                                                           */
+/* Detailed discription of possible flags in file `./methods/x_debug.h'      */
+/*                                                                           */
+/* Debugging information is written into the log file.                       */
+/* It only works if additionally UNUR_ENABLE_LOGGING is defined (see above). */
+
+#define UNUR_DEBUGFLAG_DEFAULT   UNUR_DEBUG_INIT
+
+/*---------------------------------------------------------------------------*/
+/* Warnings and error messages.                                              */
+/*                                                                           */
+/* UNURAN produces a lot of (more or less useful) warnings and error         */
+/* messages. These three compiler switches controll their output.            */
+
+/* Enable warnings and error messages.                                       */
+/* #undef this macro to suppress warnings and error messages.                */
+
+#define UNUR_WARNINGS_ON  1
+
+/* Write warnings and error messages into log file.                          */
+/* It only works if additionally UNUR_ENABLE_LOGGING is defined (see above). */
+/* #undef this macro to suppress output into log file.                       */
+
+#define UNUR_ENABLE_LOGFILE  1
+
+/* Write warnings and error messages to stderr.                              */
+/* #undef this macro to suppress output on stderr.                           */
+/*  #define UNUR_ENABLE_STDERR  1 */
+
+/* Notice that if neither UNUR_ENABLE_LOGFILE nor UNUR_ENABLE_STDERR is      */
+/* defined, then there are no warnings and error messages at all.            */
+/* However then it is recommend _not_ to define UNUR_WARNINGS_ON, either.    */
+
+/*---------------------------------------------------------------------------*/
+/* Check for invalide NULL pointer.                                          */
+/*                                                                           */
+/* UNURAN expects that every generator object is not NULL. Thus the user     */
+/* is responsible to check the result of each unur_init() call!!             */
+/*                                                                           */
+/* However checking against an invalid NULL pointer can be switched on for   */
+/* each pointer that occurs by defining  UNUR_ENABLE_CHECKNULL.              */
+
+/*  #define UNUR_ENABLE_CHECKNULL 1 */
+
+
+/*---------------------------------------------------------------------------*/
+/* Debugging tools.                                                          */
+/* (for development only. there is no need to set these flags unless         */
+/* changes are made in the library.)                                         */
+
+/* use magic cookies to validate type of pointer */
+#define UNUR_COOKIES  1
+
+/*****************************************************************************
+ *  Compile time parameters for generators.                                  *
+ *****************************************************************************/
+
+/*---------------------------------------------------------------------------*/
+/* Distribution objects.                                                     */
+
+/* Maximal number of parameters for the PDF of a distribution.               */
+/* (It must be at least 5!)                                                  */
+#define UNUR_DISTR_MAXPARAMS  5
+
+/* Maximal size of automatically created probability vectors.                */
+#define UNUR_MAX_AUTO_PV    100000
+
+
+/*****************************************************************************
+ *  Interface for uniform random number generators.                          *
+ *****************************************************************************/
+
 /*---------------------------------------------------------------------------*/
 /* Invoke uniform random number generator.                                   */
 /*                                                                           */
@@ -44,22 +158,38 @@
 /*                                                                           */
 /* UNUR_URNG_FVOID:                                                          */
 /*     Use a pointer to the routine without an argment, i.e.                 */
-/*        double urng(void);                                                 */
+/*        double uniform(void);                                              */
 /*     E.g., the uniform generator included in UNURAN.                       */ 
 /*                                                                           */
-/* UNUR_URNG_GENERIC:                                                        */
-/*     Use a pointer to the routine with a void pointer as argment, i.e.     */
-/*        double urng(void *status);                                         */
-/*     Notice that you MUST call the unur_set_default_urng() to set a        */
-/*     default uniform generators!                                           */
-/*                                                                           */
 /* UNUR_URNG_PRNG:                                                           */
-/*     Use a pointer to a routine from the plab library                      */
+/*     Use a pointer to a uniform RNG object from the `prng' library.        */
 /*     (see http://random.mat.sbg.ac.at/ftp/pub/software/gen/ or             */
-/*     or http://statistik.wu-wien.ac.at/prng/)                              */
+/*     or http://statistik.wu-wien.ac.at/prng/).                             */
 /*                                                                           */
+/* UNUR_URNG_RNGSTREAM:                                                      */
+/*     Use a pointer to a uniform RNG object from Pierre L'Ecuyer's          */
+/*     `RngStream' library for multiple multiple independent streams of      */
+/*     pseudo-random numbers.                                                */
+/*     (see http://www.iro.umontreal.ca/~lecuyer/myftp/streams00/c/)         */
+/*                                                                           */
+/* UNUR_URNG_GSL:                                                            */
+/*     Use a pointer to a uniform RNG object from the GNU Scientific         */
+/*     Library (GSL).                                                        */
+/*     (see http://www.gnu.org/software/gsl/)                                */
+/*                                                                           */
+/* UNUR_URNG_GENERIC:                                                        */
+/*     This a generic interface with limited support.                        */
+/*     It uses a structure to store both a function call of type             */
+/*     `double urng(void*)' and a void pointer to the parameter list.        */
+/*     Both must be set directly using the structure (there are currently    */
+/*     no calls that support this uniform RNG type).                         */
+/*     To avoid a segmentation fault the UNURAN build-in uniform RNGs are    */
+/*     as defaults (although these do not need a parameter list).            */
+/*     See below for more details.                                           */
+/*                                                                           */
+
 /*---------------------------------------------------------------------------*/
-/* define the possible compiler switches                                     */
+/* Define the possible compiler switches.                                    */
 #define UNUR_URNG_FVOID      1   /* use type `double urng(void)'             */
 #define UNUR_URNG_PRNG       2   /* use prng-3.x                             */
 #define UNUR_URNG_RNGSTREAM  3   /* use RngStream                            */
@@ -69,10 +199,10 @@
 
 /* Set type of uniform random number generator.                              */
 /*  #define UNUR_URNG_TYPE UNUR_URNG_FVOID */
-/*  #define UNUR_URNG_TYPE UNUR_URNG_PRNG */
+#define UNUR_URNG_TYPE UNUR_URNG_PRNG
 /*  #define UNUR_URNG_TYPE UNUR_URNG_RNGSTREAM */
 /*  #define UNUR_URNG_TYPE UNUR_URNG_GSL */
-#define UNUR_URNG_TYPE UNUR_URNG_GENERIC
+/*  #define UNUR_URNG_TYPE UNUR_URNG_GENERIC */
 
 /*---------------------------------------------------------------------------*/
 #if UNUR_URNG_TYPE == UNUR_URNG_FVOID
@@ -241,113 +371,6 @@ typedef double (_unur_urng_voidptr)(void*);      /* cast function pointer */
 /*---------------------------------------------------------------------------*/
 #endif  /* UNUR_URNG_TYPE */
 /*---------------------------------------------------------------------------*/
-
-/*---------------------------------------------------------------------------*/
-/* Default name of log file.                                                 */
-/*                                                                           */
-/* Use "stdout" to write all infos to stdout.                                */
-/* If no log file should be used, #undef the macro UNUR_ENABLE_LOGGING below.*/
-
-#define UNUR_LOG_FILE "unuran.log"
-/*  #define UNUR_LOG_FILE "stdout" */
-
-
-/*---------------------------------------------------------------------------*/
-/* Define this macro to switch on writing information about the              */
-/* generator into log file.                                                  */
-/*                                                                           */
-/* If no log file should be used at all, #undef this macro.                  */
-
-#define UNUR_ENABLE_LOGGING 1
-
-
-/*---------------------------------------------------------------------------*/
-/* Every message about a generator that is written into the log file or      */
-/* or to the stderr starts with an identifier. Every generator has its own   */
-/* unique identifier (well if there are not more than 999 generators).       */
-/* It is composed by the generator type, followed by a dot and three digits. */
-/* Building such a generator id can be disabled by undefining the following  */
-/* macro. If it is disabled the generator type (a constant string) is used   */
-/* for the identifier (which is not unique any more if more than one         */
-/* generator of the same type is created).                                   */
-
-#define UNUR_ENABLE_GENID  1
-
-
-/*---------------------------------------------------------------------------*/
-/* Set default flag for debugging of generators:                             */
-/*                                                                           */
-/*   UNUR_DEBUG_OFF    ... switch off debugging information                  */
-/*   UNUR_DEBUG_INIT   ... pameters and structure of generator only          */
-/*   UNUR_DEBUG_SETUP  ... information for setup step                        */
-/*   UNUR_DEBUG_ADAPT  ... trace adaptive steps                              */ 
-/*   UNUR_DEBUG_SAMPLE ... trace sampling                                    */
-/*   UNUR_DEBUG_ALL    ... write all available debugging information         */
-/*                                                                           */
-/* Detailed discription of possible flags in file `./methods/x_debug.h'      */
-/*                                                                           */
-/* Debugging information is written into the log file.                       */
-/* It only works if additionally UNUR_ENABLE_LOGGING is defined (see above). */
-
-#define UNUR_DEBUGFLAG_DEFAULT   UNUR_DEBUG_INIT
-
-
-/*---------------------------------------------------------------------------*/
-/* Warnings and error messages.                                              */
-/*                                                                           */
-/* UNURAN produces a lot of (more or less useful) warnings and error         */
-/* messages. These three compiler switches controll their output.            */
-
-/* Enable warnings and error messages.                                       */
-/* #undef this macro to suppress warnings and error messages.                */
-
-#define UNUR_WARNINGS_ON  1
-
-/* Write warnings and error messages into log file.                          */
-/* It only works if additionally UNUR_ENABLE_LOGGING is defined (see above). */
-/* #undef this macro to suppress output into log file.                       */
-
-#define UNUR_ENABLE_LOGFILE  1
-
-/* Write warnings and error messages to stderr.                              */
-/* #undef this macro to suppress output on stderr.                           */
-/*  #define UNUR_ENABLE_STDERR  1 */
-
-/* Notice that if neither UNUR_ENABLE_LOGFILE nor UNUR_ENABLE_STDERR is      */
-/* defined, then there are no warnings and error messages at all.            */
-/* However then it is recommend _not_ to define UNUR_WARNINGS_ON, either.    */
-
-
-/*---------------------------------------------------------------------------*/
-/* Check for invalide NULL pointer.                                          */
-/*                                                                           */
-/* UNURAN expects that every generator object is not NULL. Thus the user     */
-/* is responsible to check the result of each unur_init() call!!             */
-/*                                                                           */
-/* However checking against an invalid NULL pointer can be switched on for   */
-/* each pointer that occurs by defining  UNUR_ENABLE_CHECKNULL.              */
-
-/*  #define UNUR_ENABLE_CHECKNULL 1 */
-
-
-/*---------------------------------------------------------------------------*/
-/* Debugging tools.                                                          */
-/* (for development only. there is no need to set these flags unless         */
-/* changes are made in the library.)                                         */
-
-/* use magic cookies to validate type of pointer */
-#define UNUR_COOKIES  1
-
-
-/*---------------------------------------------------------------------------*/
-/* Distribution objects.                                                     */
-
-/* Maximal number of parameters for the PDF of a distribution.               */
-/* (It must be at least 5!)                                                  */
-#define UNUR_DISTR_MAXPARAMS  5
-
-/* Maximal size of automatically created probability vectors.                */
-#define UNUR_MAX_AUTO_PV    100000
 
 
 /*---------------------------------------------------------------------------*/

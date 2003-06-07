da5b7b99e361c31392e6db5db9d5764f72166649
jleydold <josef.leydold@wu.ac.at>
2003-06-07 16:35:09 +0000

    perl skript for making tex tables from make_timing_table output
    svn path=/trunk/; revision=1549

---
+++ trunk/experiments/timing2tex.pl
@@ -0,0 +1,156 @@
+#!/usr/bin/perl
+# ----------------------------------------------------------------------------
+
+my $Debug = 0;
+
+# ----------------------------------------------------------------------------
+
+use strict;
+use Getopt::Long;
+
+# ----------------------------------------------------------------------------
+# Global variables ...
+
+my $distr;
+my $method;
+my $dxm;
+my $mxd;
+
+my $null;
+
+my $tag = \$null;
+
+my $Format = "%-5.3f";
+
+#----------------------------------------------------------------------------
+# Read data ...
+
+GetOptions( 'format=s' => \$Format ) || die;
+
+while(<STDIN>) {
+    next unless $_=~/[^\s]/;
+    if (/^</) {
+	$tag = \$distr, next  if /^<\d+\s+distributions>/;
+	$tag = \$method, next if /^<\d+\s+methods>/;
+	$tag = \$mxd,next     if /^<Method x Distribution>/;
+	$tag = \$dxm,next     if /^<Distribution x Method>/;
+	$tag = \$null;
+    }
+    $$tag .= $_;
+}
+
+if ($Debug) {
+    print STDERR "distributions:\n$distr\n";
+    print STDERR "methods:\n$method\n";
+    print STDERR "dxm:\n$dxm\n";
+    print STDERR "mxd:\n$mxd\n";
+    print STDERR "ignore:\n$null\n\n";
+}
+
+unless ($distr and $method and $mxd) { die "data missing\n"; }
+
+#----------------------------------------------------------------------------
+# Print tables of distributions
+
+print
+    "\\begin{table}\n",
+    "  \\centering\n",
+    "  \\caption{Distributions for timing tests}\n",
+    "  \\label{tab:timing-distributions}\n",
+    "  \\begin{tabular}{l\@{~~~}l}\n",
+    "    \\hlinex\n",
+    "    Symbol \& Distribution \\\\\n",
+    "    \\xhlinex\n";
+
+my @lines = split /\n/, $distr;
+my $n_distr = $#lines + 1;
+foreach my $l (@lines) {
+    chomp $l;
+    (my $symbol, my $name) = split /\s+\.\.\.\s+/, $l;
+    $name =~ s/distr=\s*//;
+    print "      $symbol \& $name \\\\\n";
+}
+
+print
+    "    \\xhline\n",
+    "  \\end{tabular}\n",
+    "\\end{table}\n\n";
+
+if ($Debug) { print STDERR "number of distributions = $n_distr\n"; }
+
+#----------------------------------------------------------------------------
+# Print tables of Methods
+
+print
+    "\\begin{table}\n",
+    "  \\centering\n",
+    "  \\caption{Algorithms for timing tests}\n",
+    "  \\label{tab:timing-algorithms}\n",
+    "  \\begin{tabular}{l\@{~~~}l}\n",
+    "    \\hlinex\n",
+    "    Symbol \& Algorithm \\\\\n",
+    "    \\xhlinex\n";
+
+my @lines = split /\n/, $method;
+my $n_methods = $#lines + 1;
+foreach my $l (@lines) {
+    chomp $l;
+    (my $symbol, my $name) = split /\s+\.\.\.\s+/, $l;
+    $name =~ s/method=\s*//;
+    print "      $symbol \& $name \\\\\n";
+}
+
+print
+    "    \\xhline\n",
+    "  \\end{tabular}\n",
+    "\\end{table}\n\n";
+
+if ($Debug) { print STDERR "number of methods = $n_methods\n"; }
+
+#----------------------------------------------------------------------------
+# Print timings
+
+print
+    "\\begin{table}\n",
+    "  \\centering\n",
+    "  \\caption{Results of timing tests}\n",
+    "  \\label{tab:timing-result}\n";
+printf
+    "  \\begin{tabular}{l*{%d}{\@{~~}l}}\n", $n_distr;
+print
+    "    \\hlinex\n";
+
+my @lines = split /\n/, $mxd;
+
+my $header = shift @lines;
+$header =~ s/\s+/ \& /g;
+print
+    "          $header\n",
+    "    \\xhlinex\n";
+
+foreach my $l (@lines) {
+    chomp $l;
+    $l =~ s/^\s*//;
+    $l =~ s/\s*$//;
+    my @results = split /\s+/, $l;
+    my $m = shift @results;
+    print "      $m";
+    foreach my $t (@results) {
+	my $s = ($t =~ /\d/) ? sprintf($Format,$t) : "--";
+	printf " \& %-8s",$s;
+    }
+    print "\\\\\n";
+}
+
+print
+    "    \\xhline\n",
+    "  \\end{tabular}\n",
+    "\\end{table}\n\n";
+
+#----------------------------------------------------------------------------
+# End ...
+
+exit (0);
+
+#----------------------------------------------------------------------------
+

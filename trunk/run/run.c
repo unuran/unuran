e18f7b169b5058dd7352dbaf0f0c19f33de20082
jleydold <josef.leydold@wu.ac.at>
2003-06-19 13:47:50 +0000

    release 0.4.3
    svn path=/trunk/; revision=1555

--- trunk/run/run.c
+++ trunk/run/run.c
@@ -28,46 +28,26 @@ int main()
   UNUR_PAR *par;
   UNUR_GEN *gen;
   int i;
-  double stp[]={-2,-1,0,1,2,3};
-
   double x;
 
-/*    double fpm[]={0.3,5.,-1.,0}; */
-  double fpm[]={5.,0.3,-1.,0};
+  double fpm[]={0.01,2.};
 
   unur_set_default_debug(~0u);
 
   distr = unur_distr_beta(fpm,2);
 
-  par = unur_hinv_new(distr);
-  unur_hinv_set_u_resolution(par,1.e-10);
-  unur_hinv_set_order(par,3);
-/*    unur_hinv_set_boundary(par,1,2); */
-/*    unur_hinv_set_cpoints(par,stp,6); */
-/*    unur_hinv_set_max_intervals(par,1000); */
-
+  par = unur_cstd_new(distr);
   gen = unur_init(par);
 
-  for (i=0;i<10;i++)
-    printf("%g\n",unur_sample_cont(gen));
+  for (i=0;i<1000;i++)
+    unur_sample_cont(gen);
 
-/*    unur_hinv_chg_truncated(gen,1,1.001); */
-
-/*    for (i=0;i<10;i++) */
-/*      printf("%g\n",unur_sample_cont(gen)); */
-
-  printf("N = %d\n",unur_hinv_get_n_intervals(gen));
+  unur_test_chi2(gen,100,0,20,3,stdout);
 
   unur_distr_free(distr);
   unur_free(gen);
 
-
-  x = atof("");
-
-  printf("atof = %g\n",x);
-
-
-/*    unur_run_tests(par,RUN_TESTS); */
+/*    unur_run_tests(par,UNUR_TEST_CHI2); */
 
   return 0;
 

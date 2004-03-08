dac4e4114b0a081d26d58cae47f67b30af9192cd
jleydold <josef.leydold@wu.ac.at>
2004-03-08 20:17:01 +0000

    not required functions deleted
    svn path=/trunk/; revision=1623

--- trunk/src/utils/matrix_source.h
+++ trunk/src/utils/matrix_source.h
@@ -3,14 +3,12 @@
 /* header file for matrix.c	*/
 /********************************/
 
-double *_unur_matrix_cholesky_decomposition( int dim, const double *S );
-/* The Cholesky factor L of a variance-covariance matrix S is computed */
+int _unur_matrix_cholesky_decomposition (int dim, const double *S, double *L );
+/* The Colesky factor L of a variance-covariance matrix S is computed: S = LL' */
 
-int _unur_matrix_invert_matrix(int dim, double *A, double detmin, double *Ainv, double *det);
+int _unur_matrix_invert_matrix (int dim, double *A, double detmin, double *Ainv, double *det);
+/* Calculates the inverse matrix (by means of LU decomposition).             */
 /* Calculates the inverse matrix when |det(A)| > detmin */
 
-double _unur_matrix_quadratic_form(int dim, double *x, double *A);
-/* Calculates the result of x'Ax */
-
-void _unur_matrix_debug(int dim, double *M, char *info, char *genid); 
+void _unur_matrix_debug(int dim, double *M, const char *info, const char *genid);
 /* Writes the matrix-elements to the log file */

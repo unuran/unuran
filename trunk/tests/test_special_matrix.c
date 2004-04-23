9aaf3270f9aa1178be8353d78e613199e7dfa991
kara <kara@localhost>
2004-04-23 11:04:50 +0000

    absolute and relative error calculations
    svn path=/trunk/; revision=1784

--- trunk/tests/test_special_matrix.c
+++ trunk/tests/test_special_matrix.c
@@ -19,6 +19,9 @@ void _unur_test_set_matrix_2(int dim, double *M);
 /* dimension of test matrix */
 static int dim = 50;
 
+/* max tolerable absolute error */
+static double error_absolute_max = 1e-12;
+
 /* number of failed tests */
 static int n_failed = 0;
 
@@ -77,8 +80,12 @@ void _unur_test_matrix() {
   double *values;
   double *vectors;
 
+  double error_absolute; 
+  double error_relative; 
+ 
   int error_counter;
-  
+  char error_char;
+ 
   M=malloc(dim*dim*sizeof(double));
   values=malloc(dim*sizeof(double));
   vectors=malloc(dim*dim*sizeof(double));
@@ -88,17 +95,29 @@ void _unur_test_matrix() {
   error_counter = 0;
   ret = _unur_matrix_eigensystem(dim, M, values, vectors);
   if (ret == UNUR_SUCCESS) {
-    fprintf(TESTLOG, " #   \t   Eigenvalue \t\t   Expected \t\tDifference \n"); 
+    fprintf(TESTLOG, " #   \t   Eigenvalue \t\t   Expected \t\t Abs. Error \t Rel. Error\n"); 
     for (i=0; i<dim; i++) {
-      fprintf(TESTLOG, "%02d : \t%18.15f   \t%18.15f  \t%g\n", i+1,  
-      2.*values[i]/(dim+1),  (double) (i+1) , 2.*values[i]/(dim+1) - (i+1));
-      if ( _unur_FP_approx(2.*values[i]/(dim+1), i+1) ) {
-        /* eigenvalue is ok */
-      } 
-      else {
+      error_absolute = 2.*values[i]/(dim+1) - (i+1) ;  
+      error_relative = error_absolute / (i+1.);
+
+      if (error_absolute > error_absolute_max) { 
         /* eigenvalue is not ok */
-	error_counter++;
+        error_char = '*';
+        error_counter++;
       }
+      else
+      {
+        /* eigenvalue is ok */
+        error_char = ' ';
+      }
+
+      fprintf(TESTLOG, "%02d : \t%18.15f   \t%18.15f  \t% 8.4e \t% 8.4e  %c\n", 
+              i+1,  
+              2.*values[i]/(dim+1),  
+              (double) (i+1) , 
+              error_absolute,
+	      error_relative,
+	      error_char);
     }
     if (error_counter==0) {printf("[Eigensystem --> ok]");}
     else printf("[Eigensystem --> failed at %d eigenvalues]", error_counter);
@@ -106,7 +125,9 @@ void _unur_test_matrix() {
   else {
     printf("[Eigensystem --> failed]");
   }
- 
+
+  n_failed = error_counter;
+
 #undef idx1
 }
 
@@ -118,12 +139,12 @@ int main()
 {
 
   /* open log file for unuran and set output stream for unuran messages */
-  if ( (UNURANLOG = fopen( "test_special_unuran.log","w" )) == NULL )
+  if ( (UNURANLOG = fopen( "t_special_matrix_unuran.log","w" )) == NULL )
     exit (EXIT_FAILURE);
   unur_set_stream( UNURANLOG );
                                                                                                            
   /* open log file for testing */
-  if ( (TESTLOG = fopen( "test_special_matrix.log","w" )) == NULL )
+  if ( (TESTLOG = fopen( "t_special_matrix.log","w" )) == NULL )
     exit (EXIT_FAILURE);
                                                                                                            
   /* write header into log file */
@@ -138,19 +159,23 @@ int main()
   /* run tests */
   _unur_test_matrix();
 
-  /* close files */
-  fclose (UNURANLOG);
-  fclose (TESTLOG);
-                                                                                                           
-  /* end */
+
+  /* write tail into log file and some info to the console */
+  fprintf(TESTLOG,"\n======================================================\n\n");
   if (n_failed > 0) {
     printf("[Matrix --> failed]\n");
-    exit (EXIT_FAILURE);
+    fprintf(TESTLOG,"%d tests FAILED\n", n_failed);
   }
   else {
     printf("[Matrix --> ok]\n");
-    exit (EXIT_SUCCESS);
+    fprintf(TESTLOG,"All tests PASSED\n");
   }
+
+  /* close files */
+  fclose (UNURANLOG);
+  fclose (TESTLOG);
+  
+  /* end */
   return 0;
 }
 

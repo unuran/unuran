b229b093581639f059add361a456769e51337be4
kara <kara@localhost>
2004-04-30 08:56:42 +0000

    reverting changes since version/revision 1.4
    svn path=/trunk/; revision=1821

--- trunk/src/distr/cont.c
+++ trunk/src/distr/cont.c
@@ -41,7 +41,6 @@
 #include <unur_source.h>
 #include <distributions/unur_stddistr.h>
 #include <parser/functparser_source.h>
-#include <utils/fminmax_source.h>	
 #include "distr_source.h"
 #include "distr.h"
 #include "cont.h"
@@ -88,6 +87,11 @@ void _unur_distr_cont_free( struct unur_distr *distr );
 static int _unur_distr_cont_find_mode( struct unur_distr *distr );
 /*---------------------------------------------------------------------------*/
 /* find mode of unimodal univariate PDF numerically                          */
+/*---------------------------------------------------------------------------*/
+
+static double fmaxbr( UNUR_FUNCT_CONT *f, const UNUR_DISTR *distr, double a, double b, double c, double tol );
+/*---------------------------------------------------------------------------*/
+/* find maximum of function with Brent's algorithm.                          */
 /*---------------------------------------------------------------------------*/
 
 /*****************************************************************************/
@@ -1476,6 +1480,19 @@ _unur_distr_cont_find_mode( struct unur_distr *distr )
      /*----------------------------------------------------------------------*/
      /* find mode of univariate continuous distribution numerically          */
      /*                                                                      */
+     /* This is achieved by the following steps:                             */
+     /* -- Determine a interval containing the mode and a third              */
+     /*    point within ( x0< x1 < x2 )                                      */
+     /*    ++ Determine a region where to search the mode; this will be the  */
+     /*       interval [mode-100, mode+100] if this is no contrdiction to    */
+     /*       the given domain;  `mode' is the best known approx to the mode */
+     /*    ++ Find a point in the interval with a positve pdf                */
+     /*       This is done by two geometric sequences of MAX_SRCH elements   */
+     /*       and find two other points within the given domain              */
+     /*    ++ Unbounded domains: refine x0, x1, x2 until:                    */
+     /*       pdf(x0) < pdf(x1) > pdf(x2)                                    */
+     /* -- invoke a maximization-routine to determine the mode               */
+     /*                                                                      */
      /* parameters:                                                          */
      /*   distr ... pointer to distribution object                           */
      /*                                                                      */
@@ -1484,9 +1501,19 @@ _unur_distr_cont_find_mode( struct unur_distr *distr )
      /*   error code   ... on error                                          */
      /*----------------------------------------------------------------------*/
 {
+#define MAX_SRCH (100)
+ 
+  int i;
 
-  struct unur_funct_generic fs;
-  double mode;
+  double x[3];   /* mode (and x[2]) should be between x[0] and  x[2] ...*/   
+  double fx[3];  /* ... and the respective funtion values */
+  double mode;   /* (approximative) mode of the distribution  */
+  double mode_l; /* lower bound for mode search */
+  double mode_u; /* upper bound for mode search */
+  double step;
+
+  int unbound_left; 
+  int unbound_right; 
 
   /* check arguments */
   CHECK_NULL( distr, UNUR_ERR_NULL );
@@ -1496,10 +1523,184 @@ _unur_distr_cont_find_mode( struct unur_distr *distr )
     return UNUR_ERR_DISTR_DATA;
   }
 
-  fs.f = (UNUR_FUNCT_GENERIC*) DISTR.pdf;
+  /* first guess for mode */
+  mode = (_unur_FP_is_infinity(DISTR.mode)) ? 0. : DISTR.mode;
 
-  mode = _unur_util_find_max(fs, DISTR.domain[0], DISTR.domain[1], DISTR.mode);
+  /* determine where to look for the mode */
+  
+  /* unbounded domain */
+  if ( _unur_FP_is_minus_infinity(DISTR.domain[0]) &&
+       _unur_FP_is_infinity(DISTR.domain[1]) ){
 
+    unbound_left = 1;
+    unbound_right = 1;
+
+    x[1]  = mode;
+    fx[1] = DISTR.pdf(x[1], distr);    
+    mode_l = mode - 100.0;
+    mode_u = mode + 100.0;
+
+  }
+  /* domain unbounded on the right */
+  else if ( ! _unur_FP_is_minus_infinity(DISTR.domain[0]) &&
+              _unur_FP_is_infinity(DISTR.domain[1]) ){
+
+    unbound_left = 0;
+    unbound_right = 1;
+
+    if ( mode >= DISTR.domain[0] ){
+      x[1]  = mode;
+      fx[1] = DISTR.pdf(x[1], distr);
+      mode_l = DISTR.domain[0];
+      mode_u = 2 * mode - DISTR.domain[0];
+    }
+    else{
+      x[1] = DISTR.domain[0] + 100.0;
+      fx[1] = DISTR.pdf(x[1], distr);
+      mode_l = DISTR.domain[0];
+      mode_u = x[1] + 100.0;
+    }
+
+  }
+  /* domain unbounded on the left */
+  else if ( _unur_FP_is_minus_infinity(DISTR.domain[0]) &&
+          ! _unur_FP_is_infinity(DISTR.domain[1]) ){
+
+    unbound_left = 1;
+    unbound_right = 0;
+
+    if ( mode <= DISTR.domain[1] ){
+      x[1]  = mode;
+      fx[1] = DISTR.pdf(x[1], distr);
+      mode_l = DISTR.domain[1] - 2 * mode;
+      mode_u = DISTR.domain[1];
+    }
+    else{
+      x[1] = DISTR.domain[1] - 100.0;
+      fx[1] = DISTR.pdf(x[1], distr);
+      mode_l = x[1] - 100.0;
+      mode_u = DISTR.domain[1];      
+    }
+
+  }
+  /* domain is bounded */
+  else {  
+
+    unbound_left = 0;
+    unbound_right = 0;
+
+    if ( mode >= DISTR.domain[0] && mode <= DISTR.domain[1] ){
+      x[1]  = mode;
+      fx[1] = DISTR.pdf(x[1], distr);
+    }
+    else{
+      x[1] = DISTR.domain[0]/2.0 + DISTR.domain[1]/2.0;
+      fx[1] = DISTR.pdf(x[1], distr);
+    }
+    mode_l = DISTR.domain[0];
+    mode_u = DISTR.domain[1];
+
+  }
+
+  mode = x[1];  /* not exact mode -- best guess */
+
+
+  /* find point with pdf > 0.0 -- max MAX_SRCH trials */
+
+  /* search on the left side */
+  step = pow(x[1]-mode_l, 1.0/MAX_SRCH);
+  i = 0;  
+  while (i <= MAX_SRCH && _unur_FP_same(0.0, fx[1]) ){
+    x[1]  = mode - pow(step, i);
+    fx[1] = DISTR.pdf(x[1], distr);
+    i++;
+  }
+	 
+  /* search on the right side */
+  if( _unur_FP_same(0.0, fx[1]) ){
+    step = pow(mode_u-x[1], 1.0/MAX_SRCH);
+    i = 0;
+    while (i <= MAX_SRCH && _unur_FP_same(0.0, fx[1]) ){
+      x[1]  = mode + pow(step, i);
+      fx[1] = DISTR.pdf(x[1], distr);
+      i++;
+    }
+  }
+  
+  /* no success -- exit routine  */   
+  if( _unur_FP_same(fx[1], 0.0) )
+     return UNUR_ERR_DISTR_DATA;  /* can't find mode in flat region  */
+
+  /* x[1] has pdf > 0 or routines already terminated */ 
+
+
+
+  /* determine 3 points in the given domain --
+     at least one with pdf > 0                  */
+  if ( unbound_left ){
+
+    x[2] = x[1];       fx[2] = fx[1];
+    x[1] = x[2] - 1.0; fx[1] = DISTR.pdf(x[1], distr);
+    x[0] = x[2] - 2.0; fx[0] = DISTR.pdf(x[0], distr);
+
+  }
+  else if ( unbound_right ){
+
+    x[0] = x[1];       fx[0] = fx[1];
+    x[1] = x[0] + 1.0; fx[1] = DISTR.pdf(x[1], distr);
+    x[2] = x[0] + 2.0; fx[2] = DISTR.pdf(x[2], distr);
+
+  }
+  else{      /* bounded */
+
+    x[0] = DISTR.domain[0];  fx[0] = DISTR.pdf(x[0], distr);
+    x[2] = DISTR.domain[1];  fx[2] = DISTR.pdf(x[2], distr);
+
+    if ( _unur_FP_same(x[1], DISTR.domain[0])  ||
+         _unur_FP_same(x[1], DISTR.domain[1]) ){
+      x[1]  = DISTR.domain[0]/2.0 + DISTR.domain[1]/2.0; 
+      fx[1] = DISTR.pdf(x[1], distr);
+    }
+    
+  }
+  /* points x[i] with their function values determined */
+
+
+  /* find interval containing the mode of the distribution */
+
+
+  step = 1.0;
+  if ( unbound_right ){
+    while(fx[0] <= fx[1] && fx[1] <= fx[2]){ /* on the left side of the mode */
+
+      step *= 2.0;
+      x[0]  = x[1]; fx[0] = fx[1];
+      x[1]  = x[2]; fx[1] = fx[2];
+      x[2] += step; fx[2] = DISTR.pdf(x[2], distr);   
+    }
+  }
+
+  step = 1.0;  /* reset step size */
+  if ( unbound_left ){
+    while(fx[0] >= fx[1] && fx[1] >= fx[2]){ /* on the right side of the mode */
+
+      step *= 2.0;
+      x[2]  = x[1]; fx[2] = fx[1];
+      x[1]  = x[0]; fx[1] = fx[0];
+      x[0] -= step; fx[0] = DISTR.pdf(x[0], distr);
+
+    }
+  }
+
+  /* now: the mode is between x[0] and x[2]   */
+
+/*    printf("x0: %f, fx0: %e\n", x[0], fx[0]); */
+/*    printf("x1: %f, fx1: %e\n", x[1], fx[1]); */
+/*    printf("x2: %f, fx2: %e\n", x[2], fx[2]); */
+
+  /** TODO: FLT_MIN must be much larger than DBL_MIN **/
+
+  mode = fmaxbr( DISTR.pdf, distr, x[0], x[2], x[1], FLT_MIN );
   if (!(_unur_FP_is_infinity( mode )) ){
     /* mode successfully computed */
     DISTR.mode = mode;
@@ -1515,10 +1716,192 @@ _unur_distr_cont_find_mode( struct unur_distr *distr )
   /* o.k. */
   return UNUR_SUCCESS;
 
+#undef MAX_SRCH
 } /* end of _unur_distr_cont_find_mode() */
 
 /*---------------------------------------------------------------------------*/
 
+/*
+ *****************************************************************************
+ *	    		    C math library                                   *
+ * function FMINBR - one-dimensional search for a function minimum           *
+ *			  over the given range                               *
+ *                                                                           *
+ * Author: Oleg Keselyov.                                                    *
+ *                                                                           *
+ * modified by Josef Leydold (documentation unchanged)                       *
+ *                                                                           *
+ * Input                                                                     *
+ *	double fminbr(f, distr, a,b,c,tol)                                   *
+ *	double a; 			Minimum will be seeked for over      *
+ *	double b;  			a range [a,b], a being < b.          *
+ *      double c;                       c within (a,b) is first guess        *
+ *	double (*f)(double x);		Name of the function whose minimum   *
+ *					will be seeked for                   *
+ *	double tol;			Acceptable tolerance for the minimum *
+ *					location. It have to be positive     *
+ *					(e.g. may be specified as EPSILON)   *
+ *                                                                           *
+ * Output                                                                    *
+ *	Fminbr returns an estimate for the minimum location with accuracy    *
+ *	3*SQRT_EPSILON*abs(x) + tol.                                         *
+ *	The function always obtains a local minimum which coincides with     *
+ *	the global one only if a function under investigation being          *
+ *	unimodular.                                                          *
+ *	If a function being examined possesses no local minimum within       *
+ *	the given range, Fminbr returns 'a' (if f(a) < f(b)), otherwise      *
+ *	it returns the right range boundary value b.                         *
+ *                                                                           *
+ * Algorithm                                                                 *
+ *	G.Forsythe, M.Malcolm, C.Moler, Computer methods for mathematical    *
+ *	computations. M., Mir, 1980, p.202 of the Russian edition            *
+ *                                                                           *
+ *	The function makes use of the "gold section" procedure combined with *
+ *	the parabolic interpolation.                                         *
+ *	At every step program operates three abscissae - x,v, and w.         *
+ *	x - the last and the best approximation to the minimum location,     *
+ *	    i.e. f(x) <= f(a) or/and f(x) <= f(b)                            *
+ *	    (if the function f has a local minimum in (a,b), then the both   *
+ *	    conditions are fulfiled after one or two steps).                 *
+ *	v,w are previous approximations to the minimum location. They may    *
+ *	coincide with a, b, or x (although the algorithm tries to make all   *
+ *	u, v, and w distinct). Points x, v, and w are used to construct      *
+ *	interpolating parabola whose minimum will be treated as a new        *
+ *	approximation to the minimum location if the former falls within     *
+ *	[a,b] and reduces the range enveloping minimum more efficient than   *
+ *	the gold section procedure.                                          *
+ *	When f(x) has a second derivative positive at the minimum location   *
+ *	(not coinciding with a or b) the procedure converges superlinearly   *
+ *	at a rate order about 1.324                                          *
+ *                                                                           *
+ *****************************************************************************/
+
+/* in case of any error INFINITY is returned */
+
+#define SQRT_EPSILON  (1.e-7)           /* tolerance for relative error      */
+#define MAXIT         (1000)            /* maximum number of iterations      */
+
+double
+fmaxbr(f_invest, distr, a, b, c, tol)   /* An estimate to the min location   */
+     UNUR_FUNCT_CONT *f_invest;         /* Function under investigation      */
+     const UNUR_DISTR *distr;           /* distribution object with params   */
+     double a;                          /* Left border | of the range	     */
+     double b;                          /* Right border| the min is seeked   */
+     double c;                          /* first guess for the min           */
+     double tol;                        /* Acceptable tolerance              */
+{
+#define f(x) (-(*f_invest)((x),distr))  /* minimize negative function        */
+  
+  int i;
+
+  double x,v,w;                         /* Abscissae, descr. see above       */
+  double fx;                            /* f(x)                              */
+  double fv;                            /* f(v)                              */
+  double fw;                            /* f(w)                              */
+  const double r = (3.-sqrt(5.0))/2;    /* Gold section ratio                */
+
+  /* check arguments */
+  CHECK_NULL(distr,INFINITY);
+  CHECK_NULL(f_invest,INFINITY);
+  COOKIE_CHECK(distr,CK_DISTR_CONT,INFINITY);
+  if ( tol < 0. || b <= a || c <= a || b <= c) {
+    _unur_error(distr->name,UNUR_ERR_SHOULD_NOT_HAPPEN,"");
+    return INFINITY;
+  }
+
+
+  /* Origially the third point was computed by golden section. In the        */
+  /* modified version it is given as point `c' by the calling function.      */
+  /* v = a + r*(b-a);  fv = f(v);        First step - always gold section    */
+
+  v = c;  fv = f(v);                    /* First step */
+  x = v;  w = v;
+  fx=fv;  fw=fv;
+
+  for(i=0; i < MAXIT; i++) {            /* Main iteration loop	*/
+    double range = b-a;                 /* Range over which the minimum is   */
+                                        /* seeked for		             */
+    double middle_range = (a+b)/2;
+    double tol_act =                    /* Actual tolerance                  */
+		SQRT_EPSILON*fabs(x) + tol/3;
+    double new_step;                    /* Step at this iteration            */
+       
+    if( fabs(x-middle_range) + range/2 <= 2*tol_act )
+      return x;                         /* Acceptable approx. is found       */
+
+                                        /* Obtain the gold section step      */
+    new_step = r * ( x<middle_range ? b-x : a-x );
+
+
+                                 /* Decide if the interpolation can be tried */
+    if( fabs(x-w) >= tol_act ) {        /* If x and w are distinct           */
+                                        /* interpolatiom may be tried	     */
+      register double p;                /* Interpolation step is calculated  */
+      register double q;                /* as p/q; division operation        */
+                                        /* is delayed until last moment      */
+      register double t;
+
+      t = (x-w) * (fx-fv);
+      q = (x-v) * (fx-fw);
+      p = (x-v)*q - (x-w)*t;
+      q = 2*(q-t);
+
+      if( q>(double)0 )                 /* q was calculated with the         */
+	p = -p;                         /* opposite sign; make q positive    */
+      else                              /* and assign possible minus to	p    */
+	q = -q;
+
+      if( fabs(p) < fabs(new_step*q) && /* If x + p/q falls in [a,b] not too */
+	  p > q*(a-x+2*tol_act)      && /* close to a and b, and isn't too   */
+	  p < q*(b-x-2*tol_act)  )      /* large, it is accepted.            */
+	new_step = p/q;                 /* If p/q is too large then the	     */
+                                        /* gold section procedure can reduce */
+                                        /* [a,b] range to more extent.       */
+    }
+
+    if( fabs(new_step) < tol_act ) {    /* Adjust the step to be not less    */
+      if( new_step > (double)0 )        /* than tolerance.                   */
+	new_step = tol_act;
+      else
+	new_step = -tol_act;
+    }
+                                 /* Obtain the next approximation to min     */
+    {                            /* and reduce the enveloping range.         */
+      register double t = x + new_step;	/* Tentative point for the min       */
+      register double ft = f(t);
+
+      if( ft <= fx ) {                  /* t is a better approximation	     */
+	if( t < x )   
+	  b = x;                        /* Reduce the range so that          */
+	else                            /* t would fall within it            */
+	  a = x;
+      
+	v = w;  w = x;  x = t;          /* Assign the best approx to x	     */
+	fv=fw;  fw=fx;  fx=ft;
+      }
+      else {                            /* x remains the better approx       */
+	if( t < x )
+	  a = t;                        /* Reduce the range enclosing x      */
+	else
+	  b = t;
+      
+        if( ft <= fw || w==x ) {
+	  v = w;  w = t;
+	  fv=fw;  fw=ft;
+        }
+        else if( ft<=fv || v==x || v==w ) {
+	  v = t;
+	  fv=ft;
+        }
+      }
+    }                   /* ----- end-of-block ----- */
+  }                /* ===== End of for loop ===== */
+
+  /* maximal number of interations exceeded */
+  return INFINITY;
+
+#undef f
+} /* end of fmaxbr() */
 
 #undef MAXIT
 #undef SQRT_EPSILON

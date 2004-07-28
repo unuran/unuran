580db6eefa1de55e209e13149163c7f3ff1d317e
jleydold <josef.leydold@wu.ac.at>
2004-07-28 14:16:16 +0000

    run.c
    svn path=/trunk/; revision=1913

--- trunk/run/run.c
+++ trunk/run/run.c
@@ -16,75 +16,62 @@
 #include <unuran.h>
 #include <unuran_tests.h>
 
-#define RUN_TESTS       (~0x0u)
-
-/*---------------------------------------------------------------------------*/
-
-double pmf(int k, const UNUR_DISTR *distr)
-{
-  double fpm[]={1,2,3,4,5,6,5,3,6,7,5,4,3,4,6,7,5,3,4,3,8,6,5,4,7,7,2,2,1,4,7,4,3,7,3,2};
-
-  if (k<0 || k>35) return 0.;
-  else return fpm[k];
-}
-
-/*---------------------------------------------------------------------------*/
-
-double cdf(int k, const UNUR_DISTR *distr)
-{
-  double fpm[]={1,2,3,4,5,6,5,3,6,7,5,4,3,4,6,7,5,3,4,3,8,6,5,4,7,7,2,2,1,4,7,4,3,7,3,2};
-  double sum;
-  int j;
-
-  if (k<0) return 0.;
-  if (k>=35) return 1.;
-
-  for (sum=0.,j=0; j<=k; j++)
-    sum += fpm[j];
-
-  return sum/158.; 
-}
-
-/*---------------------------------------------------------------------------*/
+/* #define RUN_TESTS       (~0x0u) */
+#define RUN_TESTS       UNUR_TEST_SAMPLE
 
 /*---------------------------------------------------------------------------*/
 
 int main()
 {
+#define dim (3)
   UNUR_DISTR *distr;
   UNUR_PAR *par;
   UNUR_GEN *gen;
-  int i;
-/*    double x; */
 
-  double fpm[]={1,2,3,4,5,6,5,3,6,7,5,4,3,4,6,7,5,3,4,3,8,6,5,4,7,7,2,2,1,4,7,4,3,7,3,2};
+  double umin[]={-1,-1,-1};
+  double umax[]={1,1,1};
+
+  /*
+  int i;
+  double mean[dim], covar[dim*dim];
+  UNUR_DISTR *covar_distr = unur_distr_correlation(dim);
+  UNUR_GEN *covar_gen = unur_init(unur_mcorr_new(covar_distr));
+  UNUR_GEN *mean_gen = unur_str2gen("normal(5,1)");
+  unur_sample_matr(covar_gen,covar);
+  for (i=0; i<dim; i++) 
+    mean[i] = unur_sample_cont(mean_gen);
+  distr = unur_distr_multinormal(dim,mean,covar);
+  unur_distr_free(covar_distr);
+  unur_free(covar_gen);
+  unur_free(mean_gen); 
+  */
 
   unur_set_default_debug(~0u);
 
-  distr = unur_distr_discr_new();
-  unur_distr_discr_set_domain(distr,0,35);
-/*    unur_distr_discr_set_pv(distr,fpm,36); */
-/*    unur_distr_discr_set_pmf(distr,pmf); */
-  unur_distr_discr_set_cdf(distr,cdf);
-  unur_distr_discr_set_pmfsum(distr,158);
-
-  par = unur_dss_new(distr);
+/*   distr = unur_distr_multinormal(dim,mean,covar); */
+  distr = unur_distr_multinormal(dim,NULL,NULL);
+  par = unur_vnrou_new(distr);
+  unur_vnrou_set_rect_u(par,umin,umax);
+  unur_vnrou_set_rect_v(par,1);
   gen = unur_init(par);
 
-  for (i=0;i<1000;i++)
-    printf("%d ",unur_sample_discr(gen));
-  printf("\n");
+  unur_test_printsample( gen, 3, 3, stdout);
+  unur_test_count_urn( gen, 1000, 1, stdout );
 
-  unur_distr_free(distr);
+  unur_test_chi2( gen, 10, 1000, 10, 2, stdout );
+
+
+/*   unur_sample_matr(gen,x); */
+
+/*   unur_run_tests(par,UNUR_TEST_SAMPLE & UNUR_TEST_CHI2); */
+/*   unur_run_tests(par,UNUR_TEST_SAMPLE & UNUR_TEST_CHI2); */
+/*   unur_run_tests(par,RUN_TESTS); */
+
   unur_free(gen);
-
-/*    unur_run_tests(par,UNUR_TEST_CHI2); */
-/*    unur_run_tests(par,RUN_TESTS); */
-
-/*    unur_test_chi2(gen,100,0,20,3,stdout); */
-
-  return 0;
-
+  unur_distr_free(distr);
+  
+    return 0;
+#undef dim
 }
 
 /*---------------------------------------------------------------------------*/

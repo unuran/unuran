cae0978c693a4038a7434f515d86fe10093047d3
jleydold <josef.leydold@wu.ac.at>
2004-08-05 09:27:34 +0000

    added script for running valgrind (quick&dirty hack)
    svn path=/trunk/; revision=1957

---
+++ trunk/scripts/run_valgrind.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# check arguments 
+test -z $1 && echo "Argument missing" && exit 1
+
+VALGRINDOPTIONS="-v --leak-check=yes --leak-resolution=high --num-callers=40 --show-reachable=yes"
+
+PROG=`echo $1 | sed -e "s#\./##"`
+
+LOGFILENAME="valgrind-${PROG}"
+
+echo "run valgrind on ${PROG} ..."
+
+valgrind --logfile=${LOGFILENAME} ${VALGRINDOPTIONS} ./${PROG}
+
+exit 0

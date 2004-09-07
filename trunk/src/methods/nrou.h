6bc8bfcdeba09612fb1cab6ddfe826b7a74969aa
jleydold <josef.leydold@wu.ac.at>
2004-09-07 18:45:45 +0000

    updated docu; added description for generalized RoU method
    svn path=/trunk/; revision=1990

--- trunk/src/methods/nrou.h
+++ trunk/src/methods/nrou.h
@@ -51,40 +51,57 @@
    =REF  [HLD04: Sect.2.4]
 
    =DESCRIPTION
-      NROU is an implementation of the ratio-of-uniforms method
-      which uses (minimal) bounding rectangles, see
-      @ref{Ratio-of-Uniforms}. The coordinates of this rectangles are
-      given by 
+      NROU is an implementation of the (generalized) ratio-of-uniforms
+      method which uses (minimal) bounding rectangles, see
+      @ref{Ratio-of-Uniforms}. It uses a positive control parameter
+      @i{r} to adjust the algorithms. Large values of @i{r} increase
+      the class of distributions for which the method works at the
+      expense of a higher rejection constant. For computations reasons
+      @unurmath{r=1} (this is the default) should be used if possible.
+
+      For the special case with @i{r=1} the coordinates of the minimal
+      bounding rectangles are given by 
 
       @display
-      @unurmath{v^+ = \sup\limits_{x}       \sqrt{f(x)},} @*
-      @unurmath{u^- = \inf\limits_{x} (x-c) \sqrt{f(x)},} @*
-      @unurmath{u^+ = \sup\limits_{x} (x-c) \sqrt{f(x)}.}
+      @unurmath{v^+ = \sup\limits_{x}       \sqrt{PDF(x)},} @*
+      @unurmath{u^- = \inf\limits_{x} (x-c) \sqrt{PDF(x)},} @*
+      @unurmath{u^+ = \sup\limits_{x} (x-c) \sqrt{PDF(x)}.}
       @end display
 
       where @i{c} is the center of the distribution.
-      These bounds can either be given directly, or these are computed
-      automatically by means of an numerical routine.
-      Of course this can fail, especially when this rectangle is not
-      bounded. 
+      For other values of @i{r} we have
+      @display
+      @unurmath{v^+ = \sup\limits_{x}         (PDF(x))^{1/(r+1)},} @*
+      @unurmath{u^- = \inf\limits_{x} (x-\mu) (PDF(x))^{r/(r+1)},} @*
+      @unurmath{u^+ = \sup\limits_{x} (x-\mu) (PDF(x))^{r/(r+1)}.}
+      @end display
+
+      These bounds can be given directly. Otherwise these are computed
+      automatically by means of a (slow) numerical routine.
+      Of course this routine can fail, especially when this rectangle
+      is not bounded. 
 
       It is important to note that the algorithm works with 
-      @unurmath{PDF(x-center)} instead of 
-      @unurmath{PDF(x)}, i.e. the bounding rectangle that have to be
-      provided are for the @unurmath{PDF(x-center)}.
+      @unurmath{PDF(x-center)} instead of @unurmath{PDF(x)}.
       This is important as otherwise the acceptance region can become
       a very long and skinny ellipsoid along a diagonal of the (huge)
       bounding rectangle.
 
    =HOWTOUSE
       For using the NROU method UNURAN needs the PDF of the
-      distribution. 
-      The bounding rectangle can be given by the unur_vnrou_set_u()
-      and unur_vnrou_set_v() calls. If these are not called then the
-      minimal bounding rectangle is computed automatically. Using 
-      unur_vnrou_set_verify() and unur_vnrou_chg_verify() one can run
-      the sampling algorithm in a checking mode, i.e., in every cycle
-      of the rejection loop it is checked whether the used 
+      distribution. A bounding rectangle can be given by the
+      unur_vnrou_set_u() and unur_vnrou_set_v() calls. 
+
+      @emph{Important:} The bounding rectangle that has to be
+      provided is the function @unurmath{PDF(x-center)}!
+
+      If the coordinates of the bounding rectangle are provided by the
+      user then the minimal bounding rectangle is computed
+      automatically. 
+
+      By means of unur_vnrou_set_verify() and unur_vnrou_chg_verify()
+      one can run the sampling algorithm in a checking mode, i.e., in
+      every cycle of the rejection loop it is checked whether the used
       rectangle indeed enclosed the acceptance region of the
       distribution. When in doubt (e.g., when it is not clear whether
       the numerical routine has worked correctly) this can be used to
@@ -117,6 +134,9 @@ int unur_nrou_set_u( UNUR_PAR *parameters, double umin, double umax );
    For @unurmath{T_c}-concave distributions with @unurmath{c=-1/2} it
    should work.
 
+   @emph{Important:} The bounding rectangle that has to be
+   provided is the function @unurmath{PDF(x-center)}!
+
    Default: not set.
 */
 
@@ -138,7 +158,7 @@ int unur_nrou_set_r( UNUR_PAR *parameters, double r );
    
    @emph{Notice}: This parameter must satisfy @var{r}>0.
    Setting to a nonpositive value is ignored and in this case the
-   default value value is used instead.
+   default value is used instead.
 
    Default: @code{1}.
 */
@@ -160,7 +180,7 @@ int unur_nrou_set_verify( UNUR_PAR *parameters, int verify );
 /* 
    Turn verifying of algorithm while sampling on/off.
 
-   If the condition PDF(@i{x}) <= hat(@i{x}) is
+   If the condition @unurmath{PDF(x) \leq hat(x)} is
    violated for some @i{x} then @code{unur_errno} is set to
    @code{UNUR_ERR_GEN_CONDITION}. However notice that this might
    happen due to round-off errors for a few values of

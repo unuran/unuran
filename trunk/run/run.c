0721081076d734aa012efe5c296e97da8cfc98ec
jleydold <josef.leydold@wu.ac.at>
2004-09-10 12:23:20 +0000

    *** empty log message ***
    svn path=/trunk/; revision=2018

--- trunk/run/run.c
+++ trunk/run/run.c
@@ -27,60 +27,14 @@ int main()
   UNUR_DISTR *distr;
   UNUR_PAR *par;
   UNUR_GEN *gen;
-
-  double umin[]={-1,-1,-1};
-  double umax[]={2,2,2};
-  
-  double mean[]  = { 1., 2., 3. };
-  double covar[] = { 2., 2., 1.,   2., 4., 3.,   1., 3., 3. };
-
-  /*
-  int i;
-  double mean[dim], covar[dim*dim];
-  UNUR_DISTR *covar_distr = unur_distr_correlation(dim);
-  UNUR_GEN *covar_gen = unur_init(unur_mcorr_new(covar_distr));
-  UNUR_GEN *mean_gen = unur_str2gen("normal(5,1)");
-  unur_sample_matr(covar_gen,covar);
-  for (i=0; i<dim; i++) 
-    mean[i] = unur_sample_cont(mean_gen);
-  distr = unur_distr_multinormal(dim,mean,covar);
-  unur_distr_free(covar_distr);
-  unur_free(covar_gen);
-  unur_free(mean_gen); 
-  */
+  double timing_setup, timing_sample;
 
   unur_set_default_debug(~0u);
 
-/*   
-  distr = unur_distr_multinormal(dim,NULL,NULL);
-*/
-
-  distr = unur_distr_multinormal(dim,mean,covar); 
-  par = unur_varou_new(distr);
-
-/*
-  unur_vnrou_set_u(par,umin,umax);
-  unur_vnrou_set_v(par,1);
-  unur_vnrou_set_r(par,2.);
-*/
-
-  gen = unur_init(par);
-
-  unur_test_printsample( gen, 3, 3, stdout);
-  unur_test_count_urn( gen, 1000, 1, stdout );
-
-/*
-  unur_test_chi2( gen, 10, 1000, 10, 2, stdout );
-*/
-
-/*   unur_sample_matr(gen,x); */
-
-/*   unur_run_tests(par,UNUR_TEST_SAMPLE & UNUR_TEST_CHI2); */
-/*   unur_run_tests(par,UNUR_TEST_SAMPLE & UNUR_TEST_CHI2); */
-/*   unur_run_tests(par,RUN_TESTS); */
+  par = unur_unif_new(NULL);
+  gen = unur_test_timing( par, 6, &timing_setup, &timing_sample, TRUE, stdout);
 
   unur_free(gen);
-  unur_distr_free(distr);
   
     return 0;
 #undef dim

3c79be69e0204eac2995801b00bedbf78a40cbbe
jleydold <josef.leydold@wu.ac.at>
2004-09-13 15:03:15 +0000

    *** empty log message ***
    svn path=/trunk/; revision=2024

--- trunk/makewin.sh
+++ trunk/makewin.sh
@@ -10,8 +10,11 @@ MINGWPATH="${MINGWHOME}/i386-mingw32msvc/bin"
 export CC="${MINGWHOME}/bin/i386-mingw32msvc-gcc"
 export STRIP="${MINGWPATH}/strip"
 export AR="${MINGWPATH}/ar"
+export AS="${MINGWPATH}/as"
 export RANLIB="${MINGWPATH}/ranlib"
 export NM="${MINGWPATH}/nm"
+export LD="${MINGWPATH}/ld"
+export DLLTOOL="${MINGWPATH}/dlltool"
 
 # run configure in its own build directory
 test -d win || mkdir win;

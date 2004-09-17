0278bbc39896c81f0dcf72fb1410ced6f9065cb8
kara <kara@localhost>
2004-09-17 11:14:01 +0000

    vector structures and functions
    svn path=/trunk/; revision=2026

---
+++ trunk/src/utils/vector.c
@@ -0,0 +1,67 @@
+/*****************************************************************************
+ *                                                                           *
+ *          UNURAN -- Universal Non-Uniform Random number generator          *
+ *                                                                           *
+ *****************************************************************************
+ *                                                                           *
+ *   FILE: vector.c                                                          *
+ *                                                                           *
+ *   Routines for computations with vectors (arrays).                        *
+ *                                                                           *
+ *****************************************************************************
+ 
+ *****************************************************************************
+ *                                                                           *
+ *   Dept. for Statistics, University of Economics, Vienna, Austria          *
+ *                                                                           *
+ *   This program is free software; you can redistribute it and/or modify    *
+ *   it under the terms of the GNU General Public License as published by    *
+ *   the Free Software Foundation; either version 2 of the License, or       *
+ *   (at your option) any later version.                                     *
+ *                                                                           *
+ *   This program is distributed in the hope that it will be useful,         *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ *   GNU General Public License for more details.                            *
+ *                                                                           *
+ *   You should have received a copy of the GNU General Public License       *
+ *   along with this program; if not, write to the                           *
+ *   Free Software Foundation, Inc.,                                         *
+ *   59 Temple Place, Suite 330, Boston, MA 02111-1307, USA                  *
+ *                                                                           *
+ *****************************************************************************/
+				                                                                                    
+/*--------------------------------------------------------------------------*/
+
+#include <unur_source.h>
+#include "vector_source.h"
+
+/*---------------------------------------------------------------------------*/
+
+/* allocate memory for new vector structure */
+UNUR_VECTOR *unur_vector_new(int dim)
+{
+  UNUR_VECTOR *v;
+  v = _unur_xmalloc(sizeof(UNUR_VECTOR));
+
+  v->dim = dim;
+  v->x = _unur_xmalloc(dim*sizeof(double));
+
+  return v;
+}
+
+/*--------------------------------------------------------------------------*/
+
+/* free allocated memory used by vector structure */
+void unur_vector_free(UNUR_VECTOR *v)
+{
+  if (v) {
+     if (v->x) free(v->x);
+     free(v);
+     v=NULL;
+  }
+}
+			  
+
+/*--------------------------------------------------------------------------*/
+

09cb49590272defb4d5377b4203c8c7d5e482d73
kara <kara@localhost>
2004-09-30 12:24:17 +0000

    added initial bounding rectangle calculations
    svn path=/trunk/; revision=2043

--- trunk/src/methods/varou.c
+++ trunk/src/methods/varou.c
@@ -85,13 +85,29 @@
 
 /*---------------------------------------------------------------------------*/
 
+/* convergence parameters for the hooke optimization algorithm */
+
+#define VAROU_HOOKE_RHO 0.5 
+#define VAROU_HOOKE_EPSILON 1e-7 
+#define VAROU_HOOKE_MAXITER 10000
+
+/* scaling factor of computed minimum boundary rectangle */
+/* after we have computed the boundary rectangle (0, vmax)x(umin[d], umax[d])*/
+/* we scale the obtained boundaries with this factor, i.e. :                 */
+/* vmax = vmax * ( 1+ VAROU_RECT_SCALING)                                    */
+/* umin[d] = umin[d] - (umax[d]-umin[d])*VAROU_RECT_SCALING/2.               */
+/* umax[d] = umax[d] + (umax[d]-umin[d])*VAROU_RECT_SCALING/2.               */
+#define VAROU_RECT_SCALING 1e-4
+
+/*---------------------------------------------------------------------------*/
+
+
 /*---------------------------------------------------------------------------*/
 
 static struct unur_gen *_unur_varou_init( struct unur_par *par );
 /*---------------------------------------------------------------------------*/
 /* Initialize new generator.                                                 */
 /*---------------------------------------------------------------------------*/
-
 
 static struct unur_gen *_unur_varou_create( struct unur_par *par );
 /*---------------------------------------------------------------------------*/
@@ -104,10 +120,16 @@ static void  _unur_varou_sample_check( struct unur_gen *gen, double *vec );
 /* sample from generator.                                                    */
 /*---------------------------------------------------------------------------*/
 
+static int _unur_varou_rectangle( struct unur_gen *gen );
+/*---------------------------------------------------------------------------*/
+/* compute (minimal) bounding rectangle.                                     */
+/*---------------------------------------------------------------------------*/
+
 static void _unur_varou_free( struct unur_gen *gen);
 /*---------------------------------------------------------------------------*/
 /* destroy generator object.                                                 */
 /*---------------------------------------------------------------------------*/
+
 
 #ifdef UNUR_ENABLE_LOGGING
 /*---------------------------------------------------------------------------*/
@@ -124,6 +146,13 @@ static void _unur_varou_debug_init( const struct unur_gen *gen );
 
 /*---------------------------------------------------------------------------*/
 /*   Auxilliary Routines                                                     */
+/*---------------------------------------------------------------------------*/
+
+double _unur_varou_aux_vmax(double *x, void *p );
+double _unur_varou_aux_umin(double *x, void *p );
+double _unur_varou_aux_umax(double *x, void *p );
+/*---------------------------------------------------------------------------*/
+/* Auxiliary functions used in the computation of the bounding rectangle     */
 /*---------------------------------------------------------------------------*/
 
 static double _unur_varou_f( struct unur_gen *gen, double *u, double v);
@@ -323,6 +352,13 @@ _unur_varou_init( struct unur_par *par )
     return NULL; 
   }
 
+  /* compute bounding rectangle */
+  if (_unur_varou_rectangle(gen)!=UNUR_SUCCESS) {
+    free(par); _unur_varou_free(gen);
+    return NULL;
+  }
+
+
   /* compute initial triangulation ... etc  */
   /* ...  */
 
@@ -377,10 +413,16 @@ _unur_varou_create( struct unur_par *par )
   gen->destroy = _unur_varou_free;
   gen->clone = _unur_varou_clone;
 
+  /* allocate memory for u-boundary arrays */
+  GEN.umin = _unur_xmalloc( PAR.dim * sizeof(double)); /* bounding rectangle */
+  GEN.umax = _unur_xmalloc( PAR.dim * sizeof(double)); /* bounding rectangle */
 
   /* copy parameters into generator object */
   GEN.dim   = PAR.dim;              /* dimension */
   
+  /* get center of the distribution */
+  GEN.center = unur_distr_cvec_get_center(gen->distr);
+
   /* initialize parameters */
 
   /* return pointer to (almost empty) generator object */
@@ -415,8 +457,16 @@ _unur_varou_clone( const struct unur_gen *gen )
   /* create generic clone */
   clone = _unur_generic_clone( gen, GENTYPE );
 
+  /* allocate memory for u-arrays and center */
+  CLONE.umin = _unur_xmalloc( GEN.dim * sizeof(double));
+  CLONE.umax = _unur_xmalloc( GEN.dim * sizeof(double));
+  
+  /* copy parameters into clone object */
+  memcpy(CLONE.umin, GEN.umin, GEN.dim * sizeof(double));
+  memcpy(CLONE.umax, GEN.umax, GEN.dim * sizeof(double));
+
   /* copy data */
-  /* ... */
+  CLONE.center = unur_distr_cvec_get_center(clone->distr);
   
   return clone;
 
@@ -472,7 +522,7 @@ _unur_varou_sample_check( struct unur_gen *gen, double *vec )
 
   dim = GEN.dim;
   
-  /* TODO : CHANGE THIS !!!!!!! */  
+  /* TODO : CHANGE THIS !!!!!!! These are only dummy random values */  
     for (d=0; d<dim; d++) {
       vec[d] = _unur_call_urng(gen->urng) ;
     }
@@ -506,6 +556,9 @@ _unur_varou_free( struct unur_gen *gen )
   SAMPLE = NULL;   /* make sure to show up a programming error */
 
   /* free memory */
+  if (GEN.umin) free(GEN.umin); 
+  if (GEN.umax) free(GEN.umax);
+
   _unur_generic_free(gen);
 
 } /* end of _unur_varou_free() */
@@ -513,13 +566,61 @@ _unur_varou_free( struct unur_gen *gen )
 /*****************************************************************************/
 /**  Auxilliary Routines                                                    **/
 /*****************************************************************************/
+ 
+double
+_unur_varou_aux_vmax(double *x, void *p )
+    /*----------------------------------------------------------------------*/
+    /* Auxiliary function used in the computation of the bounding rectangle */
+    /*----------------------------------------------------------------------*/
+{
+
+    struct unur_gen *gen;
+    gen = p; /* typecast from void* to unur_gen* */
+
+    return -pow( _unur_cvec_PDF((x),(gen->distr)) ,
+                         1./(1.+ GEN.dim) );
+
+} /* end of _unur_varou_aux_vmax() */
+		   
+/*---------------------------------------------------------------------------*/
+
+double
+_unur_varou_aux_umin(double *x, void *p)
+    /*----------------------------------------------------------------------*/
+    /* Auxiliary function used in the computation of the bounding rectangle */
+    /*----------------------------------------------------------------------*/
+{
+    struct unur_gen *gen;
+
+    gen = p; /* typecast from void* to unur_gen* */
+
+    return (x[GEN.aux_dim] - GEN.center[GEN.aux_dim])
+	              * pow( _unur_cvec_PDF((x),(gen->distr)),
+		                1. / (1.+ GEN.dim) );
+
+} /* end of _unur_varou_aux_umin() */
+					      
+/*---------------------------------------------------------------------------*/
+
+double
+_unur_varou_aux_umax(double *x, void *p)
+    /*----------------------------------------------------------------------*/
+    /* Auxiliary function used in the computation of the bounding rectangle */
+    /*----------------------------------------------------------------------*/
+{
+    return (- _unur_varou_aux_umin(x,p)) ;
+
+} /* end of _unur_varou_aux_umin() */
+
+/*---------------------------------------------------------------------------*/
+
 
 double _unur_varou_f( struct unur_gen *gen, double *u, double v) 
-/*                                                            */
-/* return calculated value of PDF(u_1/v, u_2/v, ..., u_dim/v) */
-/*                                                            */
-/* UNUR_INFINITY is returned when |v|<UNUR_EPSILON            */
-/*                                                            */
+    /*                                                            */
+    /* return calculated value of PDF(u_1/v, u_2/v, ..., u_dim/v) */
+    /*                                                            */
+    /* UNUR_INFINITY is returned when |v|<UNUR_EPSILON            */
+    /*                                                            */
 {
   int d, dim; /* index used in dimension loops (0 <= d < dim) */
   double *uv; /* u/v vector     */
@@ -541,15 +642,15 @@ double _unur_varou_f( struct unur_gen *gen, double *u, double v)
   return f;
 }
 
-/*****************************************************************************/
+/*---------------------------------------------------------------------------*/
 
 double 
 _unur_varou_F( struct unur_gen *gen, double *u, double v) 
-/*                                                                        */
-/* return calculated value of v^(1+dim) - PDF(u_1/v, u_2/v, ..., u_dim/v) */
-/*                                                                        */
-/* UNUR_INFINITY is returned when |v|<UNUR_EPSILON                        */
-/*                                                                        */
+    /*                                                                        */
+    /* return calculated value of v^(1+dim) - PDF(u_1/v, u_2/v, ..., u_dim/v) */
+    /*                                                                        */
+    /* UNUR_INFINITY is returned when |v|<UNUR_EPSILON                        */
+    /*                                                                        */
 {
   double F;   /* function value */
  
@@ -560,15 +661,15 @@ _unur_varou_F( struct unur_gen *gen, double *u, double v)
   return F;
 }
 
-/*****************************************************************************/
+/*---------------------------------------------------------------------------*/
 
 double * 
 _unur_varou_dF( struct unur_gen *gen, double *u, double v )
-/*                                                                        */
-/* return the calculated value of the gradient of F() at the point (u,v)  */
-/*                                                                        */
-/* NULL is returned when |v|<UNUR_EPSILON                                 */
-/*                                                                        */
+    /*                                                                        */
+    /* return the calculated value of the gradient of F() at the point (u,v)  */
+    /*                                                                        */
+    /* NULL is returned when |v|<UNUR_EPSILON                                 */
+    /*                                                                        */
 {
   int d, dim; /* index used in dimension loops (0 <= d < dim) */
   double *uv; /* u/v vector     */
@@ -606,6 +707,173 @@ _unur_varou_dF( struct unur_gen *gen, double *u, double v )
   return dF;
 }
 
+/*---------------------------------------------------------------------------*/
+
+int
+_unur_varou_rectangle( struct unur_gen *gen )
+     /*----------------------------------------------------------------------*/
+     /* compute universal bounding rectangle                                 */
+     /*                                                                      */
+     /* parameters:                                                          */
+     /*   gen ... pointer to generator object                                */
+     /*                                                                      */
+     /* return:                                                              */
+     /*   UNUR_SUCCESS ... on success                                        */
+     /*   error code   ... on error                                          */
+     /*----------------------------------------------------------------------*/
+{ 
+
+  struct unur_funct_vgeneric faux; /* function to be minimized/maximized    */
+  double *xstart, *xend, *xumin, *xumax; /* coordinate arrays used in maximum/minimum calculations */
+  int d, dim; /* index used in dimension loops (0 <= d < dim) */
+  int hooke_iters_vmax;  /* actual number of min/max iterations = return value of hooke()*/
+  int hooke_iters_umin;  /* actual number of min/max iterations = return value of hooke()*/
+  int hooke_iters_umax;  /* actual number of min/max iterations = return value of hooke()*/
+  double scaled_epsilon; /* to be used in the hooke algorithm */
+
+  /* check arguments */
+  CHECK_NULL( gen, UNUR_ERR_NULL );
+  COOKIE_CHECK( gen,CK_VAROU_GEN, UNUR_ERR_COOKIE );
+
+  /* dimension of the distribution */
+  dim = GEN.dim;
+
+  /* allocate memory for the coordinate vectors */
+  xstart = _unur_xmalloc(dim * sizeof(double));
+  xend   = _unur_xmalloc(dim * sizeof(double));
+  xumin  = _unur_xmalloc(dim * sizeof(double));
+  xumax  = _unur_xmalloc(dim * sizeof(double));
+  
+  /* calculation of vmax */
+    /* calculating vmax as maximum of sqrt(f(x)) in the domain */
+      faux.f = (UNUR_FUNCT_VGENERIC*) _unur_varou_aux_vmax;
+      faux.params = gen;
+
+      if (gen->distr->set & UNUR_DISTR_SET_MODE)
+          /* starting point at mode */
+          memcpy(xstart, DISTR.mode, dim * sizeof(double)); 
+      else
+          /* starting point at center */
+          memcpy(xstart, GEN.center, dim * sizeof(double)); 
+     
+      hooke_iters_vmax = _unur_hooke( faux, dim, xstart, xend, 
+				      VAROU_HOOKE_RHO, VAROU_HOOKE_EPSILON, VAROU_HOOKE_MAXITER);
+
+      GEN.vmax = -faux.f(xend, faux.params);
+      
+      if (hooke_iters_vmax >= VAROU_HOOKE_MAXITER) {
+	 scaled_epsilon = VAROU_HOOKE_EPSILON * GEN.vmax;
+	 if (scaled_epsilon>VAROU_HOOKE_EPSILON) scaled_epsilon=VAROU_HOOKE_EPSILON;
+
+         /* recalculating extremum with scaled_epsilon and new starting point */
+         memcpy(xstart, xend, dim * sizeof(double)); 
+         hooke_iters_vmax = _unur_hooke( faux, dim, xstart, xend, 
+                              VAROU_HOOKE_RHO, scaled_epsilon , VAROU_HOOKE_MAXITER);
+         GEN.vmax = -faux.f(xend, faux.params);
+         if (hooke_iters_vmax >= VAROU_HOOKE_MAXITER) {
+           _unur_warning(gen->genid , UNUR_ERR_GENERIC, "Bounding rect uncertain (vmax)");  
+         }
+      }
+
+
+  /* calculation of umin and umax */
+    
+    for (d=0; d<dim; d++) {
+
+      /* setting coordinate dimension to be used by the auxiliary functions */
+      GEN.aux_dim  = d;
+  
+      if (gen->distr->set & UNUR_DISTR_SET_MODE)
+          /* starting point at mode */
+          memcpy(xstart, DISTR.mode, dim * sizeof(double)); 
+      else
+          /* starting point at center */
+          memcpy(xstart, GEN.center, dim * sizeof(double)); 
+      
+      /*-----------------------------------------------------------------------------*/
+      /* calculation for umin */
+      
+      faux.f = (UNUR_FUNCT_VGENERIC*) _unur_varou_aux_umin;
+      faux.params = gen;
+
+      hooke_iters_umin = _unur_hooke( faux, dim, xstart, xend, 
+                           VAROU_HOOKE_RHO, VAROU_HOOKE_EPSILON, VAROU_HOOKE_MAXITER);
+      GEN.umin[d] = faux.f(xend, faux.params);
+      
+      /* storing actual endpoint in case we need a recalculation */
+      memcpy(xumin, xend, dim * sizeof(double)); 
+
+      /*-----------------------------------------------------------------------------*/
+      /* and now, an analogue calculation for umax */
+
+      faux.f = (UNUR_FUNCT_VGENERIC*) _unur_varou_aux_umax;
+      faux.params = gen;
+
+      hooke_iters_umax = _unur_hooke( faux, dim, xstart, xend, 
+                           VAROU_HOOKE_RHO, VAROU_HOOKE_EPSILON, VAROU_HOOKE_MAXITER);
+      GEN.umax[d] = -faux.f(xend, faux.params);
+      
+      /* storing actual endpoint in case we need a recalculation */
+      memcpy(xumax, xend, dim * sizeof(double)); 
+
+      /*-----------------------------------------------------------------------------*/
+      /* checking if we need to recalculate umin */
+      if (hooke_iters_umin >= VAROU_HOOKE_MAXITER) {
+	 scaled_epsilon = VAROU_HOOKE_EPSILON * (GEN.umax[d]-GEN.umin[d]);
+	 if (scaled_epsilon>VAROU_HOOKE_EPSILON) scaled_epsilon=VAROU_HOOKE_EPSILON;
+
+         /* recalculating extremum with scaled_epsilon and new starting point */
+         faux.f = (UNUR_FUNCT_VGENERIC*) _unur_varou_aux_umin;
+         faux.params = gen;
+
+         memcpy(xstart, xumin, dim * sizeof(double)); 
+         hooke_iters_umin = _unur_hooke( faux, dim, xstart, xend, 
+                              VAROU_HOOKE_RHO, scaled_epsilon , VAROU_HOOKE_MAXITER);
+         GEN.umin[d] = faux.f(xend, faux.params);
+         if (hooke_iters_umin >= VAROU_HOOKE_MAXITER) {
+           _unur_warning(gen->genid , UNUR_ERR_GENERIC, "Bounding rect uncertain (umin)");  
+         }
+      }
+
+      /* checking if we need to recalculate umax */
+      if (hooke_iters_umax >= VAROU_HOOKE_MAXITER) {
+	 scaled_epsilon = VAROU_HOOKE_EPSILON * (GEN.umax[d]-GEN.umin[d]);
+	 if (scaled_epsilon>VAROU_HOOKE_EPSILON) scaled_epsilon=VAROU_HOOKE_EPSILON;
+
+         /* recalculating extremum with scaled_epsilon and new starting point */
+         faux.f = (UNUR_FUNCT_VGENERIC*) _unur_varou_aux_umax;
+         faux.params = gen;
+        
+	 memcpy(xstart, xumax, dim * sizeof(double)); 
+         hooke_iters_umax = _unur_hooke( faux, dim, xstart, xend, 
+                              VAROU_HOOKE_RHO, scaled_epsilon , VAROU_HOOKE_MAXITER);
+         GEN.umin[d] = faux.f(xend, faux.params);
+         if (hooke_iters_umax >= VAROU_HOOKE_MAXITER) {
+           _unur_warning(gen->genid , UNUR_ERR_GENERIC, "Bounding rect uncertain (umax)");  
+         }
+      }
+    
+      /*-----------------------------------------------------------------------------*/
+      /* additional scaling of boundary rectangle */   
+      GEN.vmax = GEN.vmax * ( 1+ VAROU_RECT_SCALING);
+      GEN.umin[d] = GEN.umin[d] - (GEN.umax[d]-GEN.umin[d])*VAROU_RECT_SCALING/2.;
+      GEN.umax[d] = GEN.umax[d] + (GEN.umax[d]-GEN.umin[d])*VAROU_RECT_SCALING/2.;        
+    
+    }
+ 
+  free(xstart); free(xend); free(xumin); free(xumax);
+
+  /* o.k. */
+  return UNUR_SUCCESS;
+
+} /* end of _unur_varou_rectangle() */
+
+/*---------------------------------------------------------------------------*/
+
+
+
+
+
 /*****************************************************************************/
 /**  Debugging utilities                                                    **/
 /*****************************************************************************/
@@ -625,6 +893,7 @@ _unur_varou_debug_init( const struct unur_gen *gen )
 {
   FILE *log;
   int d, dim; /* index used in dimension loops (0 <= d < dim) */
+  double vol;
 
   /* check arguments */
   CHECK_NULL(gen,RETURN_VOID);  COOKIE_CHECK(gen,CK_VAROU_GEN,RETURN_VOID);
@@ -644,7 +913,26 @@ _unur_varou_debug_init( const struct unur_gen *gen )
   fprintf(log,"()\n%s:\n",gen->genid);
 
   /* parameters */
-  for (d=0; d<dim; d++) {}
+
+
+  /* print center */
+  _unur_matrix_print_vector( GEN.dim, GEN.center, "center =", log, gen->genid, "\t   ");
+
+  /* print bounding rectangle */
+  fprintf(log,"%s: Rectangle:",gen->genid);
+  fprintf(log,"\t[computed]\n");
+ 
+  vol = GEN.vmax;
+  fprintf(log,"%s:\tvmax = %g\n",gen->genid, GEN.vmax);
+  for (d=0; d<dim; d++) {
+    vol *= (GEN.umax[d]-GEN.umin[d]);
+    fprintf(log,"%s:\tumin[%d],umax[%d] = (%g,%g)\n",gen->genid, 
+	    d, d, GEN.umin[d], GEN.umax[d]);
+  }
+  fprintf(log,"%s:\n",gen->genid);
+  fprintf(log,"%s:\tvolume = %g\t(hat = %g)\n",gen->genid, vol, vol*(GEN.dim+1));
+  fprintf(log,"%s:\n",gen->genid);
+
 
   fprintf(log,"%s:\n",gen->genid);
 

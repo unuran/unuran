8080f0b97ae30fb48f80eeb55b18cbfd84ec26f9
jleydold <josef.leydold@wu.ac.at>
2005-02-07 22:06:59 +0000

    *** empty log message ***
    svn path=/trunk/; revision=2199

--- trunk/run/run.c
+++ trunk/run/run.c
@@ -23,25 +23,36 @@
 
 int main()
 {
-  UNUR_DISTR *distr;
+  UNUR_DISTR *distr, *marginal;
   UNUR_PAR *par;
   UNUR_GEN *gen;
 
   double rk[] = { 1., 0.5, 0.1,  0.5, 1., 0.3,  0.1, 0.3, 1. };
 
   double erc[9];
+  double fpar[1] = {5};
 
   unur_set_default_debug(~0U);
 
-  distr =unur_distr_copula(3,rk);
+  distr = unur_distr_cvec_new(3);
+  unur_distr_cvec_set_rankcorr(distr,rk);
+/*   marginal = unur_distr_uniform(NULL,0); */
+  marginal = unur_distr_gamma(fpar,1);
+  unur_distr_cvec_set_marginals(distr,marginal);
+  unur_distr_free(marginal);
+
+/*   distr = unur_distr_copula(3,rk); */
+
+
+
   par = unur_norta_new(distr);
-  gen = unur_init(par);
 
 /*   unur_run_tests( par, RUN_TESTS); */
 
-  unur_test_cvec_rankcorr( erc, gen, 100000, 2, stdout );
-
+  gen = unur_init(par);
+  unur_test_cvec_rankcorr( erc, gen, 100, 2, stdout );
   unur_free(gen);
+
   unur_distr_free(distr);
   
   return 0;

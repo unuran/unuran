b7cfe2c39db04042ed30da4541dbc847bbdc0d9f
jleydold <josef.leydold@wu.ac.at>
2005-02-09 14:48:07 +0000

    added support for suppressing messages
    svn path=/trunk/; revision=2207

--- trunk/scripts/run_valgrind.sh
+++ trunk/scripts/run_valgrind.sh
@@ -3,7 +3,9 @@
 # check arguments 
 test -z $1 && echo "Argument missing" && exit 1
 
-VALGRINDOPTIONS="-v --tool=memcheck --leak-check=yes --leak-resolution=high --num-callers=40 --show-reachable=yes"
+test -f suppressions-valgrind && SUPPRESS="--suppressions=suppressions-valgrind"
+
+VALGRINDOPTIONS="-v --tool=memcheck --leak-check=yes --leak-resolution=high --num-callers=40 --show-reachable=yes $SUPPRESS"
 
 PROG=`echo $1 | sed -e "s#\./##"`
 

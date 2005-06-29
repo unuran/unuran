9d65144a0c98b53ce9c8a3d5ff20f012f54d5ca4
kara <kara@localhost>
2005-06-29 16:10:06 +0000

    removing command line arguments
    svn path=/trunk/; revision=2375

--- trunk/run/lotto.c
+++ trunk/run/lotto.c
@@ -25,76 +25,43 @@ int SEED=163;
 
 /*---------------------------------------------------------------------------*/
 
-void printarray(char *info, int *a) {
+void printarray(int *a) {
   int  p;
   
-  printf("%s", info);
-
   for (p=0; p<P; p++) {
-
     printf("  % d", a[p]);
   }
-  
   printf("\n");
 
 }
 
 
-int main(int argc, char *argv[])
+/*---------------------------------------------------------------------------*/
+
+int main()
 {
 
   UNUR_GEN *gen=NULL;
-
-  int *a;
-  char api_str[40];
-  char info[20];
+  int *a=NULL;
+  char api_str[80];
   
   int i,p ;
   int flag_equal;
   double x;
   
-  char c;
-  
   time_t now;
   struct tm t;
 
-  /* read options */
-  while ((c = getopt(argc, argv, "p:n:h")) != -1) {
-    switch (c) {
-    case 'p':     
-      P=atol(optarg);
-      break;
-    case 'n':     
-      N=atol(optarg);
-      break;
-    
-    case 'h':     
-      printf("options\n" );
-      printf(" -n N         : default = %d  \n", N );
-      printf(" -p P         : default = %d  \n", P );
-      
-      return 0;
-      break;
-    default:
-      break;
-    }
-  }
-  
-  if (N>1 && N<100 && 
-      P>1 && P<N) {
-        a=malloc(P*sizeof(int));
-  }    
-  else {
-    return 0;
-  }
+ 
+  unur_set_default_debug(UNUR_DEBUG_OFF);
+
+  a=malloc(P*sizeof(int));
 
   time(&now);
   t = *localtime(&now);
 
   SEED = (int) (11723 +  SEED * (t.tm_sec*t.tm_min+(t.tm_sec*2.1)*t.tm_mon ));
-
   sprintf(api_str, "uniform(0,%d) & urng=mt19937(%d)", N, SEED); 
- 
   gen=unur_str2gen(api_str);
 
   if (gen==NULL) return 0; 
@@ -117,7 +84,7 @@ int main(int argc, char *argv[])
     if (flag_equal==0) p++;
   }
 
-  printarray("", a);
+  printarray(a);
   
   unur_free(gen);
   
@@ -127,5 +94,4 @@ int main(int argc, char *argv[])
 
 }
 
-/*---------------------------------------------------------------------------*/
 

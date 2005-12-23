437df7c7e79f2879ddf7fa929ec1f21d8cac7412
jleydold <josef.leydold@wu.ac.at>
2005-12-23 12:19:39 +0000

    *** empty log message ***
    svn path=/trunk/; revision=2728

--- trunk/run/run.c
+++ trunk/run/run.c
@@ -16,42 +16,41 @@
 #include <unuran.h>
 #include <unuran_tests.h>
 
-#include <hitro.h>
-
 #define RUN_TESTS       (~0x0u)
 /* #define RUN_TESTS       UNUR_TEST_SAMPLE */
 
 /*---------------------------------------------------------------------------*/
 
+double pdf( double x, const UNUR_DISTR *distr )
+{
+/*   return (1.e32 * pow(1-x*x,-50) * exp((78.043 * x - 107.415)/(1 - x*x))); */
+  return exp(log(1.e32)+  50*log(1-x*x) + ((78.043 * x - 107.415)/(1 - x*x)));
+}
+
+
+/*---------------------------------------------------------------------------*/
+
 int main()
 {
-/*   UNUR_DISTR *distr; */
+  UNUR_DISTR *distr;
   UNUR_PAR *par;
   UNUR_GEN *gen;
 
-  UNUR_DISTR *normal;
-
   unur_set_default_debug(~0U);
 
-  normal = unur_distr_multinormal(3,NULL,NULL);
+  distr = unur_distr_cont_new();
+  unur_distr_cont_set_mode(distr,0.710413);
+  unur_distr_cont_set_pdf(distr, pdf);
 
-  par = unur_hitro_new(normal);
-  unur_hitro_set_variant_random_direction(par);
-  unur_hitro_set_use_adaptiveline(par,TRUE);
-  unur_hitro_set_use_adaptiverectangle(par,TRUE);
-  unur_hitro_set_use_boundingrectangle(par,TRUE);
+  par = unur_nrou_new(distr);
 
-  unur_set_debug( par, ~0u);
-/*   unur_gibbs_set_thinning(par,3); */
-/*   unur_gibbs_set_variant_random_direction(par); */
+  gen = unur_init(par);
 
-/*   gen = unur_init(par); */
-
-  unur_run_tests(par,~0u);
+/*   unur_run_tests(par,~0u); */
 /*   unur_run_tests(par,UNUR_TEST_SAMPLE); */
   
-/*   unur_free(gen); */
-  unur_distr_free(normal);
+  unur_free(gen);
+/*   unur_distr_free(normal); */
 
   return 0;
 }

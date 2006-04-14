a4691f4391079e24278ba10478acc195d47617c8
jleydold <josef.leydold@wu.ac.at>
2006-04-14 09:25:06 +0000

    adapted valgring options to new release 3.1
    svn path=/trunk/; revision=2786

--- trunk/scripts/run_valgrind.sh
+++ trunk/scripts/run_valgrind.sh
@@ -5,7 +5,7 @@ test -z $1 && echo "Argument missing" && exit 1
 
 test -f suppressions-valgrind && SUPPRESS="--suppressions=suppressions-valgrind"
 
-VALGRINDOPTIONS="-v --tool=memcheck --leak-check=yes --leak-resolution=high --num-callers=40 --show-reachable=yes $SUPPRESS"
+VALGRINDOPTIONS="-v --tool=memcheck --leak-check=full --leak-resolution=high --num-callers=40 --show-reachable=yes --track-fds=yes $SUPPRESS"
 
 PROG=`echo $1 | sed -e "s#\./##"`
 
@@ -13,7 +13,7 @@ LOGFILENAME="valgrind-${PROG}"
 
 echo "run valgrind on ${PROG} ..."
 
-valgrind --logfile=${LOGFILENAME} ${VALGRINDOPTIONS} ./${PROG}
+valgrind --log-file=${LOGFILENAME} ${VALGRINDOPTIONS} ./${PROG}
 
 echo ""; echo "Summary:"; echo ""
 grep ERROR ${LOGFILENAME}*; grep lost ${LOGFILENAME}*

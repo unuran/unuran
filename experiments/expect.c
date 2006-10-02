daa82b38ba60fe260797ab7735412e6a11000501
jleydold <josef.leydold@wu.ac.at>
2006-10-02 08:19:53 +0000

    - updated copyright statement
    svn path=/branches/unuran-devel-root/; revision=3102

--- experiments/expect.c
+++ experiments/expect.c
@@ -4,15 +4,14 @@
  *                                                                           *
  *****************************************************************************
  *                                                                           *
- *   FILE: expect.c                                                        *
+ *   FILE: expect.c                                                          *
  *                                                                           *
  *   Routines for calculations of expectation values of integrals            *
  *                                                                           *
  *****************************************************************************
- 
- *****************************************************************************
  *                                                                           *
- *   Dept. for Statistics, University of Economics, Vienna, Austria          *
+ *   Copyright (c) 2000-2006 Wolfgang Hoermann and Josef Leydold             *
+ *   Department of Statistics and Mathematics, WU Wien, Austria              *
  *                                                                           *
  *   This program is free software; you can redistribute it and/or modify    *
  *   it under the terms of the GNU General Public License as published by    *

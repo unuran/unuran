b70599d2bb8d53d3b9c0ba3ca209d14d1cd509e7
jleydold <josef.leydold@wu.ac.at>
2006-10-02 08:04:24 +0000

    - updated copyright statement
    svn path=/branches/unuran-devel-root/; revision=3099

--- src/unur_cookies.h
+++ src/unur_cookies.h
@@ -14,11 +14,9 @@
  *          included only in source_unuran.h                                 *
  *                                                                           *
  *****************************************************************************
-     $Id$
- *****************************************************************************
  *                                                                           *
- *   Copyright (c) 2000 Wolfgang Hoermann and Josef Leydold                  *
- *   Dept. for Statistics, University of Economics, Vienna, Austria          *
+ *   Copyright (c) 2000-2006 Wolfgang Hoermann and Josef Leydold             *
+ *   Department of Statistics and Mathematics, WU Wien, Austria              *
  *                                                                           *
  *   This program is free software; you can redistribute it and/or modify    *
  *   it under the terms of the GNU General Public License as published by    *

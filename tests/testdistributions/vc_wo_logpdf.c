cbd9cee0c0ac108a4ee88979127be3d4ac758641
jleydold <josef.leydold@wu.ac.at>
2006-10-02 08:06:02 +0000

    - updated copyright statement
    svn path=/branches/unuran-devel-root/; revision=3100

--- tests/testdistributions/vc_wo_logpdf.c
+++ tests/testdistributions/vc_wo_logpdf.c
@@ -7,11 +7,9 @@
  *  Distribution objects of standard distributions without logPDF            *
  *                                                                           *
  *****************************************************************************
-     $Id: c_wo_logpdf.c 2562 2005-10-26 14:57:09Z leydold $
- *****************************************************************************
  *                                                                           *
- *   Copyright (c) 2000 Wolfgang Hoermann and Josef Leydold                  *
- *   Dept. for Statistics, University of Economics, Vienna, Austria          *
+ *   Copyright (c) 2000-2006 Wolfgang Hoermann and Josef Leydold             *
+ *   Department of Statistics and Mathematics, WU Wien, Austria              *
  *                                                                           *
  *   This program is free software; you can redistribute it and/or modify    *
  *   it under the terms of the GNU General Public License as published by    *

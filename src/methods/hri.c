2538a15cfb992e47ad53cf0ad8e8eb0c1fe17123
jleydold <josef.leydold@wu.ac.at>
2008-01-09 15:34:54 +0000

    - implemented info string for method HRI
    svn path=/trunk/; revision=3935

--- src/methods/hri.c
+++ src/methods/hri.c
@@ -63,6 +63,10 @@
 #include "hri.h"
 #include "hri_struct.h"
 
+#ifdef UNUR_ENABLE_INFO
+#  include <tests/unuran_tests.h>
+#endif
+
 /*---------------------------------------------------------------------------*/
 /* Constants                                                                 */
 
@@ -144,6 +148,13 @@ static void _unur_hri_debug_sample( const struct unur_gen *gen,
 				    double x, double p1, int i0, int i1 );
 /*---------------------------------------------------------------------------*/
 /* trace sampling.                                                           */
+/*---------------------------------------------------------------------------*/
+#endif
+
+#ifdef UNUR_ENABLE_INFO
+static void _unur_hri_info( struct unur_gen *gen, int help );
+/*---------------------------------------------------------------------------*/
+/* create info string.                                                       */
 /*---------------------------------------------------------------------------*/
 #endif
 
@@ -461,6 +472,11 @@ _unur_hri_create( struct unur_par *par )
 
   /* initialize variables */
   GEN->left_border = 0.;             /* left border of domain                 */
+
+#ifdef UNUR_ENABLE_INFO
+  /* set function for creating info string */
+  gen->info = _unur_hri_info;
+#endif
 
   /* return pointer to (almost empty) generator object */
   return gen;
@@ -912,4 +928,68 @@ _unur_hri_debug_sample( const struct unur_gen *gen,
 
 /*---------------------------------------------------------------------------*/
 #endif   /* end UNUR_ENABLE_LOGGING */
+/*---------------------------------------------------------------------------*/
+
+
+/*---------------------------------------------------------------------------*/
+#ifdef UNUR_ENABLE_INFO
+/*---------------------------------------------------------------------------*/
+
+void
+_unur_hri_info( struct unur_gen *gen, int help )
+     /*----------------------------------------------------------------------*/
+     /* create character string that contains information about the          */
+     /* given generator object.                                              */
+     /*                                                                      */
+     /* parameters:                                                          */
+     /*   gen  ... pointer to generator object                               */
+     /*   help ... whether to print additional comments                      */
+     /*----------------------------------------------------------------------*/
+{
+  struct unur_string *info = gen->infostr;
+  struct unur_distr *distr = gen->distr;
+  int samplesize = 10000;
+  double rc;
+
+  /* generator ID */
+  _unur_string_append(info,"generator ID: %s\n\n", gen->genid);
+  
+  /* distribution */
+  _unur_string_append(info,"distribution:\n");
+  _unur_string_append(info,"   name      = %s\n", distr->name);
+  _unur_string_append(info,"   type      = continuous univariate distribution\n");
+  _unur_string_append(info,"   functions = HR\n");
+  _unur_string_append(info,"   domain    = (%g, %g)\n", DISTR.domain[0],DISTR.domain[1]);
+  _unur_string_append(info,"\n");
+
+  /* method */
+  _unur_string_append(info,"method: HRI (Hazard Rate Increasing)\n");
+  _unur_string_append(info,"\n");
+
+  /* performance */
+  _unur_string_append(info,"performance characteristics:\n");
+  rc = 0.1 * (unur_test_count_urn(gen,samplesize,0,NULL)/(samplesize/10));
+  _unur_string_append(info,"   E[#interations] = %g  [approx.]\n", rc);
+  _unur_string_append(info,"\n");
+
+  /* parameters */
+  if (help) {
+    _unur_string_append(info,"parameters:\n");
+    _unur_string_append(info,"   p0 = %g  %s\n", GEN->p0,
+  			(gen->set & HRI_SET_P0) ? "" : "[default]"); 
+    _unur_string_append(info,"\n");
+  }
+
+  /* Hints */
+  if (help) {
+    if ( !(gen->set & HRI_SET_P0))
+      _unur_string_append(info,"[ Hint: %s ]\n",
+			  "You can set the design point \"p0\" to increase performance.");
+    _unur_string_append(info,"\n");
+  }
+
+} /* end of _unur_hri_info() */
+
+/*---------------------------------------------------------------------------*/
+#endif   /* end UNUR_ENABLE_INFO */
 /*---------------------------------------------------------------------------*/

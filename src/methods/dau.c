784fe2a4ee4faa22c61658b3b922cd8903f204ee
jleydold <josef.leydold@wu.ac.at>
2008-01-10 07:55:08 +0000

    - implemented info string for method DAU
    svn path=/trunk/; revision=3938

--- src/methods/dau.c
+++ src/methods/dau.c
@@ -202,6 +202,13 @@ static void _unur_dau_debug_table( struct unur_gen *gen );
 /*---------------------------------------------------------------------------*/
 #endif
 
+#ifdef UNUR_ENABLE_INFO
+static void _unur_dau_info( struct unur_gen *gen, int help );
+/*---------------------------------------------------------------------------*/
+/* create info string.                                                       */
+/*---------------------------------------------------------------------------*/
+#endif
+
 /*---------------------------------------------------------------------------*/
 /* abbreviations */
 
@@ -463,6 +470,11 @@ _unur_dau_create( struct unur_par *par)
   GEN->urn_size = 0;
   GEN->jx = NULL;
   GEN->qx = NULL;
+
+#ifdef UNUR_ENABLE_INFO
+  /* set function for creating info string */
+  gen->info = _unur_dau_info;
+#endif
 
   /* return pointer to (almost empty) generator object */
   return gen;
@@ -860,4 +872,64 @@ _unur_dau_debug_table( struct unur_gen *gen )
 
 /*---------------------------------------------------------------------------*/
 #endif   /* end UNUR_ENABLE_LOGGING */
+/*---------------------------------------------------------------------------*/
+
+
+/*---------------------------------------------------------------------------*/
+#ifdef UNUR_ENABLE_INFO
+/*---------------------------------------------------------------------------*/
+
+void
+_unur_dau_info( struct unur_gen *gen, int help )
+     /*----------------------------------------------------------------------*/
+     /* create character string that contains information about the          */
+     /* given generator object.                                              */
+     /*                                                                      */
+     /* parameters:                                                          */
+     /*   gen  ... pointer to generator object                               */
+     /*   help ... whether to print additional comments                      */
+     /*----------------------------------------------------------------------*/
+{
+  struct unur_string *info = gen->infostr;
+  struct unur_distr *distr = gen->distr;
+
+  /* generator ID */
+  _unur_string_append(info,"generator ID: %s\n\n", gen->genid);
+  
+  /* distribution */
+  _unur_string_append(info,"distribution:\n");
+  _unur_string_append(info,"   name      = %s\n", distr->name);
+  _unur_string_append(info,"   type      = continuous univariate distribution\n");
+  _unur_string_append(info,"   functions = PV [lenght=%d%s]\n",
+		      DISTR.domain[1]-DISTR.domain[0]+1,
+		      (DISTR.pmf==NULL) ? "" : ", created from PMF");
+  _unur_string_append(info,"   domain    = (%d, %d)\n", DISTR.domain[0],DISTR.domain[1]);
+  _unur_string_append(info,"\n");
+
+  /* method */
+  _unur_string_append(info,"method: DAU (Alias-Urn)\n");
+  _unur_string_append(info,"\n");
+
+  /* performance */
+  _unur_string_append(info,"performance characteristics:\n");
+  _unur_string_append(info,"   E [#look-ups] = %g\n", 1+1./GEN->urn_factor);
+  _unur_string_append(info,"\n");
+
+  /* parameters */
+  if (help) {
+    _unur_string_append(info,"parameters:\n");
+    _unur_string_append(info,"   urnfactor = %g  %s\n", GEN->urn_factor,
+			(gen->set & DAU_SET_URNFACTOR) ? "" : "[default]");
+    _unur_string_append(info,"\n");
+  }
+
+  /* Hints */
+  /*   if (help) { */
+  /*     _unur_string_append(info,"\n"); */
+  /*   } */
+
+} /* end of _unur_dau_info() */
+
+/*---------------------------------------------------------------------------*/
+#endif   /* end UNUR_ENABLE_INFO */
 /*---------------------------------------------------------------------------*/

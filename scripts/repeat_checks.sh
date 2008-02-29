29fac52ca1ee630b5b27a69b4482981218184168
jleydold <josef.leydold@wu.ac.at>
2008-02-29 10:38:12 +0000

    - make script more flexibel:    allow running of a single test program
    svn path=/trunk/; revision=4045

--- scripts/repeat_checks.sh
+++ scripts/repeat_checks.sh
@@ -6,15 +6,17 @@ RESULTS=RESULTS
 
 RULER="-------------------------------"
 
+MAKE="make -j2"
+
 # -----------------------------------
 
 usage() {
 cat << "USAGE_END"
-Usage: repeat_checks.sh [options] <rep> 
-
-Repeat checks with different seeds <rep> times
+Usage: repeat_checks.sh [options]
 
 Options:
+  -n=N           repeat N times [default: N=10]
+  -t=TEST        run particular program TEST [default: run all tests]
   -f             run in fullcheck mode
   -h, --help     help (print this page)
 
@@ -27,6 +29,16 @@ USAGE_END
 
 for x in "$@" ; do
     case "${x}" in
+	-n=*) # number of repetitions
+	    rep=`echo "${x}" | sed -e "s/-n=//"`
+	    ;;
+	-t=*) # name of test program
+	    testname=`echo "${x}" | sed -e "s/-t=//"`
+	    if [[ ! -x "$testname" ]] ; then 
+		echo "invalid test: \"$testname\""
+		usage 1
+	    fi
+	    ;;
         -f)        # fullcheck mode
 	    export UNURANFULLCHECK=true
 	    ;;
@@ -37,9 +49,10 @@ for x in "$@" ; do
 	    echo "$0: Invalid switch \"${x}\"!."
 	    usage 1
 	    ;;
-        *)   # this should be the number of repetitions
-             rep="${x}"
-             ;;
+        *)   # invalid argument
+	    echo "$0: Invalid argument \"${x}\"!."
+	    usage 1
+	    ;;
     esac
 done
 
@@ -49,11 +62,6 @@ done
 if [[ -z "$rep" ]] ; then rep=10; fi
 
 echo "number of repetitions = $rep"
-
-# -----------------------------------
-# make
-
-MAKE="make -j2"
 
 # -----------------------------------
 # make directory with test results
@@ -74,29 +82,42 @@ echo "${RULER}" >> $RESULTS/RESULTS
 # run ...
 
 for (( i=0; i<$rep; i++)); do
+    ## create directory for results
+    mkdir -v "$RESULTS/$i"
+    ## create random seed
     SEED=`echo $RANDOM$RANDOM | cut -b1-8`
     echo $i:$SEED
     date
-    mkdir -v "$RESULTS/$i"
     echo "SEED = ${SEED}" > $RESULTS/$i/ENV
+    ## check mode
     if [[ -n "${UNURANFULLCHECK}" ]] ; then
 	echo "check mode: fullcheck" >> $RESULTS/$i/ENV
     else
 	echo "check mode: installation" >> $RESULTS/$i/ENV
     fi
+    ## set environment variables for tests
     export SEED="${SEED}"
     export UNURANSTOPWATCH=true
-    time ($MAKE check > $RESULTS/$i/RESULTS) > $RESULTS/$i/TIMING 2>&1
-    echo "move log files"
+    ## run tests
+    if [[ -z "$testname" ]] ; then 
+	time ($MAKE check >> $RESULTS/$i/RESULTS) >> $RESULTS/$i/TIMING 2>&1
+    else
+	$MAKE $testname >> $RESULTS/$i/RESULTS 2>&1
+	time ("./$testname" >> $RESULTS/$i/RESULTS) >> $RESULTS/$i/TIMING 2>&1
+    fi
+    ## move log files
     mv *.log $RESULTS/$i
+    #### (cd $RESULTS/$i; grep "PASS: " RESULTS | sed -e 's/PASS: //' | sed -e 's/$/_test.log/' | xargs -i= rm "=")
+    (cd $RESULTS/$i; grep "PASS: " RESULTS | sed -e 's/PASS: //' | sed -e 's/$/_unuran.log/' | xargs -i= rm "=")
+    ## store test results 
     echo "#${i}: SEED=${SEED}" >> $RESULTS/RESULTS
     grep "real" $RESULTS/$i/TIMING  | sed -e 's/real/time =/' >> $RESULTS/RESULTS
     tail -n 10 $RESULTS/$i/RESULTS | grep "[0-9]\+ of [0-9]\+ tests failed" >> $RESULTS/RESULTS
     tail -n 10 $RESULTS/$i/RESULTS | grep "All [0-9]\+ tests passed" >> $RESULTS/RESULTS
     grep "FAIL: " $RESULTS/$i/RESULTS | sed -e 's/FAIL:/   FAIL:/' >> $RESULTS/RESULTS
     echo "${RULER}" >> $RESULTS/RESULTS
-    ## (cd $RESULTS/$i; grep "PASS: " RESULTS | sed -e 's/PASS: //' | sed -e 's/$/_test.log/' | xargs -i= rm "=")
-    (cd $RESULTS/$i; grep "PASS: " RESULTS | sed -e 's/PASS: //' | sed -e 's/$/_unuran.log/' | xargs -i= rm "=")
+    ## print info on screen
+    echo `grep "FAIL: " $RESULTS/$i/RESULTS | sed -e 's/FAIL:/   FAIL:/'`
 done
 
 # -----------------------------------

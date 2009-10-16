5fb54a7918d7eb726a5b6be9cc3498152551ddb3
jleydold <josef.leydold@wu.ac.at>
2009-10-16 12:49:47 +0000

    - options for new valgrind version
    svn path=/trunk/; revision=4679

--- scripts/run_valgrind.sh
+++ scripts/run_valgrind.sh
@@ -5,7 +5,7 @@ test -z $1 && echo "Argument missing" && exit 1
 
 test -f suppressions-valgrind && SUPPRESS="--suppressions=suppressions-valgrind"
 
-VALGRINDOPTIONS="-v --tool=memcheck --leak-check=full --leak-resolution=high --num-callers=40 --show-reachable=yes --track-fds=yes $SUPPRESS"
+VALGRINDOPTIONS="-v --stats=yes --tool=memcheck --leak-check=full --leak-resolution=high --num-callers=40 --show-reachable=yes --track-fds=yes $SUPPRESS"
 
 PROG=`echo $1 | sed -e "s#\./##"`
 

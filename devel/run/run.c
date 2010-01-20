e248079e05d58ad8c996732a428b00818c53ef09
jleydold <josef.leydold@wu.ac.at>
2010-01-20 18:29:19 +0000

    - some experiments
    svn path=/trunk/; revision=4844

--- devel/run/run.c
+++ devel/run/run.c
@@ -21,6 +21,26 @@
 
 /*---------------------------------------------------------------------------*/
 
+double mylogpdf( double x, const UNUR_DISTR *distr )
+{
+  double bs = 0.001;
+  double bn = 0.001/3.;
+  double s  = 12.;
+
+  return ((bs-1.)*log(x)+log(1.-x)*(bn-1.)+s*(1.-x));
+}
+
+double mydlogpdf( double x, const UNUR_DISTR *distr )
+{
+  double bs = 0.001;
+  double bn = 0.001/3.;
+  double s  = 12.;
+
+  return ((bs-1.)/x - (bn-1.)/(1.-x)-s);
+}
+
+/*---------------------------------------------------------------------------*/
+
 int main()
 {
   UNUR_DISTR *distr;
@@ -28,18 +48,26 @@ int main()
   UNUR_GEN *gen;
   int i;
 
-  double fpar[] = {1.1,3.2};
-
   unur_set_default_debug(~0U);
 
-  distr = unur_distr_F(fpar,2);
-  unur_distr_cont_set_center(distr,1);
-  par = unur_pinv_new(distr);
-  unur_run_tests(par,UNUR_TEST_N_PDF|UNUR_TEST_TIME,stdout);
+  distr = unur_distr_cont_new();
+  unur_distr_cont_set_logpdf( distr, mylogpdf );
+  unur_distr_cont_set_dlogpdf( distr, mydlogpdf );
+  unur_distr_cont_set_domain( distr, 0., 0.5);
+  unur_distr_cont_set_mode( distr, 0.);
 
-  /* gen = unur_str2gen("normal & method=ninv; u_resolution=1e-10"); */
-  /* for (i=0; i<1e1; i++)  */
-  /*   unur_sample_cont(gen); */
+  par = unur_itdr_new(distr);
+  unur_itdr_set_cp(par,-0.999);
+
+  gen = unur_init(par);
+
+  if (gen == NULL) {
+     fprintf(stderr, "ERROR: cannot create generator object\n");
+     exit (EXIT_FAILURE);
+  }
+
+  for (i=1; i<100; i++)
+    printf("%g\n",unur_sample_cont(gen));
 
   return 0;
 }
